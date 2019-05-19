343750b311b384498a298a662d58b7ac3e1d0483
Chris <38708476+WChrisK@users.noreply.github.com>
2019-05-19 15:11:49 -0400

    State now drawn on the BSP visualizer, added string drawing helpers

--- BspVisualizer/Form1.cs
+++ BspVisualizer/Form1.cs
@@ -1,12 +1,5 @@
 ﻿using Helion.BSP.Builder;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BSPVisualizer
@@ -15,29 +8,78 @@ namespace BSPVisualizer
     {
         private static Font defaultFont = new Font("Arial", 16.0f);
         private static SolidBrush blackBackgroundBrush = new SolidBrush(Color.Black);
+        private static SolidBrush whiteBrush = new SolidBrush(Color.White);
+        private static SolidBrush cyanBrush = new SolidBrush(Color.Cyan);
         private static Pen whitePen = new Pen(new SolidBrush(Color.White));
         private static Pen lightGrayPen = new Pen(new SolidBrush(Color.LightGray));
         private static Pen yellowPen = new Pen(new SolidBrush(Color.Yellow));
 
         private StepwiseBspBuilder bspBuilder;
 
-        public Form1(StepwiseBspBuilder bspBuilder)
+        public Form1(StepwiseBspBuilder builder)
         {
-            this.bspBuilder = bspBuilder;
+            bspBuilder = builder;
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Draws a series of strings on one line. This allows multiple strings
+        /// with different colors to be drawn sequentially.
+        /// </summary>
+        /// <param name="g">The graphics object.</param>
+        /// <param name="font">The font to draw with.</param>
+        /// <param name="topLeft">The top left corner.</param>
+        /// <param name="colorAndStrings">A variable length array which should
+        /// be an alternation of brush, then string pairs. For example, this
+        /// can be: whitebrush, "hi", blueBrush "!"</param>
+        private void DrawStrings(Graphics g, Font font, PointF topLeft, params object[] colorAndStrings)
+        {
+            int xOffset = (int)topLeft.X;
+
+            for (int i = 0; i < colorAndStrings.Length / 2; i++)
+            {
+                SolidBrush brush = colorAndStrings[i * 2] as SolidBrush ?? whiteBrush;
+                string str = colorAndStrings[(i * 2) + 1] as string ?? "?";
+
+                SizeF size = g.MeasureString(str, font);
+                RectangleF drawBox = new RectangleF(xOffset, topLeft.Y, xOffset + size.Width, topLeft.Y + size.Height);
+                g.DrawString(str, font, brush, drawBox);
+
+                xOffset += (int)size.Width;
+            }
+        }
+
+        private void PaintBackground(Graphics g, Rectangle windowBounds)
+        {
+            g.FillRectangle(blackBackgroundBrush, windowBounds);
+        }
+
+        private void PaintShadowLines(Graphics g, Rectangle windowBounds)
+        {
+            // TODO
+        }
+
+        private void PaintCurrentState(Graphics g, Rectangle windowBounds)
+        {
+            // TODO
+        }
+
+        private void PaintTextInfo(Graphics g, Rectangle windowBounds)
+        {
+            DrawStrings(g, defaultFont, new PointF(2, windowBounds.Height - 30),
+                        whiteBrush, "State: ", 
+                        cyanBrush, bspBuilder.States.Next.ToString());
         }
 
         private void Panel1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            g.FillRectangle(blackBackgroundBrush, e.ClipRectangle);
+            Rectangle windowBounds = e.ClipRectangle;
 
-            //PaintTextInfo(g, e.ClipRectangle);
-            //PaintShadowLines(g, e.ClipRectangle);
-            //PaintCurrentState(g, e.ClipRectangle);
-
-            //g.DrawLine(oneSidedLinePen, 200, 200, 300, 300);
-            //g.DrawString("hello", defaultFont, oneSidedLineBrush, new RectangleF(100, 100, 50, 10));
+            PaintBackground(g, windowBounds);
+            PaintShadowLines(g, windowBounds);
+            PaintCurrentState(g, windowBounds);
+            PaintTextInfo(g, windowBounds);
         }
     }
 }
