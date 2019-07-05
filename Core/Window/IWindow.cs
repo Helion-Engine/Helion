6bef06654af30c40be30dfdffd6022ce7fa4f2a5
Chris <38708476+WChrisK@users.noreply.github.com>
2019-07-05 12:24:16 -0400

    Removed outdated objects that were not removed originally, included IWindow interface that should have been in originally as well

---
+++ Core/Window/IWindow.cs
@@ -0,0 +1,14 @@
+﻿using Helion.Input;
+using Helion.Render;
+using Helion.Util.Geometry;
+
+namespace Helion.Window
+{
+    public interface IWindow
+    {
+        int GetId();
+        InputEvent PollInput();
+        IRenderer GetRenderer();
+        Dimension GetDimension();
+    }
+}
