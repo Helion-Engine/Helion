310319d02c64c7778c82c09edf021d0df774494f
Chris <38708476+wchrisk@users.noreply.github.com>
2019-07-31 01:59:48 -0400

    Added an epsilon collinarity test

--- Tests/Unit/Bsp/Geometry/CollinearTrackerTest.cs
+++ Tests/Unit/Bsp/Geometry/CollinearTrackerTest.cs
@@ -62,5 +62,16 @@ namespace Helion.Test.Unit.Bsp.Geometry
             Assert.AreEqual(first, second);
             Assert.AreEqual(first, third);
         }
+
+        [TestMethod]
+        public void HasCollinearityWithinEpsilon()
+        {
+            CollinearTracker collinearTracker = new CollinearTracker(0.01);
+            
+            int first = collinearTracker.GetOrCreateIndex(new Vec2D(0, 0), new Vec2D(4, 4));
+            int second = collinearTracker.GetOrCreateIndex(new Vec2D(1, 1), new Vec2D(2.001, 1.999));
+            
+            Assert.AreEqual(first, second);
+        }
     }
 }
