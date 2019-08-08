006d8dfe767d6d2f1d4b803f79bb96f0d6348d8a
Nick <stlaurent.nicholas@gmail.com>
2019-08-07 21:03:37 -0400

    fix to MoveRepetition

--- Core/Maps/Special/LineSpecialData.cs
+++ Core/Maps/Special/LineSpecialData.cs
@@ -3,14 +3,14 @@
     public class LineSpecialData
     {
         public LineSpecialData(LineSpecialType type, ActivationType activationType, SectorMoveType moveType, MoveDirection startDirection,
-            SectorDest dest, MoveRepitition repitition, double speed)
+            SectorDest dest, MoveRepetition repetition, double speed)
         {
             LineSpecialType = type;
             ActivationType = activationType;
             SectorMoveType = moveType;
             StartDirection = startDirection;
             SectorDestination = dest;
-            MoveRepitition = repitition;
+            MoveRepetition = repetition;
             Speed = speed;
         }
 
@@ -19,7 +19,7 @@
         public SectorMoveType SectorMoveType { get; private set; }
         public MoveDirection StartDirection { get; private set; }
         public SectorDest SectorDestination { get; private set; }
-        public MoveRepitition MoveRepitition { get; private set; }
+        public MoveRepetition MoveRepetition { get; private set; }
         public double Speed { get; private set; }
     }
 }
