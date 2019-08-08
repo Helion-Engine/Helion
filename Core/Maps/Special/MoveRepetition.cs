a76605c956a1792c60b8e50c1c3be2f730281b15
Nick <stlaurent.nicholas@gmail.com>
2019-08-07 21:00:45 -0400

    fix naming, update enum for ReturnOnBlock and DelayReturn

--- Core/Maps/Special/MoveRepetition.cs
+++ Core/Maps/Special/MoveRepetition.cs
@@ -4,16 +4,18 @@
     /// Defines how sector movement repeats during it's lifetime.
     /// None - The move special will destroy when it hits it's destination. The move special will continue to try to move while being blocked by an entity.
     /// e.g. Floor raise to, Ceiling lower to specials etc.
-    /// RepeatOnce - The move special will destroy when it returns to it start position. The move special will continually flip direction while being blocked by an entity.
+    /// ReturnOnBlock - The move special will destroy when it hits it's destination. If blocked it will return to it's start position.
+    /// e.g. Door close special.
+    /// DelayReturn - The move special will destroy when it returns to it start position. Movement flips will delay. The move special will continually flip direction while being blocked by an entity.
     /// e.g. Door and lift specials.
     /// Perpetual - The move special will move back and forth until signaled to stop. The move special will continue to try to move while being blocked by an entity.
-    /// e.g. Start moving floor special.
+    /// e.g. Start moving floor perpetual special.
     /// </summary>
-    /// <remarks>
-    public enum MoveRepitition
+    public enum MoveRepetition
     {
         None,
-        RepeatOnce,
+        ReturnOnBlock,
+        DelayReturn,
         Perpetual,
     }
 }
