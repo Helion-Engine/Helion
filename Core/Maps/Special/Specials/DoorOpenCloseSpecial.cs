9a51bb6186a26bb2bf0161f62b8b76db4ffbc1e6
Nick <stlaurent.nicholas@gmail.com>
2019-08-18 07:19:40 -0400

    update soo dooropenclose will handle use during movement

---
+++ Core/Maps/Special/Specials/DoorOpenCloseSpecial.cs
@@ -0,0 +1,23 @@
+﻿using Helion.Maps.Geometry;
+using Helion.World.Physics;
+
+namespace Helion.Maps.Special.Specials
+{
+    public class DoorOpenCloseSpecial : SectorMoveSpecial
+    {
+        public DoorOpenCloseSpecial(PhysicsManager physicsManager, Sector sector, double dest, double speed, int delay)
+            : base(physicsManager, sector, sector.Floor.Z, dest, new SectorMoveData(SectorMoveType.Ceiling, MoveDirection.Up, MoveRepetition.DelayReturn, speed, delay))
+        {
+        }
+
+        public override void Use()
+        {
+            // If the delay is zero then flip the door direction
+            // Otherwise we are in the wait delay and setting the delay back to 0 will immediately bring it back down
+            // Either way we need to set delay to 0, because this effect needs to work immediately
+            if (m_delayTics == 0)
+                FlipMovementDirection();
+            m_delayTics = 0;
+        }
+    }
+}
