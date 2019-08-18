6e378af9dc0a2e8d93861b639a4eef48372a57cd
Nick <stlaurent.nicholas@gmail.com>
2019-08-18 07:22:48 -0400

    special for switch texture changes, repeat and one time use

---
+++ Core/Maps/Special/Specials/SwitchChangeSpecial.cs
@@ -0,0 +1,50 @@
+﻿using Helion.Maps.Geometry;
+using Helion.Maps.Geometry.Lines;
+
+namespace Helion.Maps.Special.Specials
+{
+    public class SwitchChangeSpecial : ISpecial
+    {
+        public Sector? Sector => null;
+
+        private SwitchManager m_manager;
+        private Line m_line;
+        private bool m_repeat;
+        private int m_switchDelayTics;
+
+        public SwitchChangeSpecial(SwitchManager manager, Line line)
+        {
+            m_manager = manager;
+            m_line = line;
+            m_repeat = line.Flags.Repeat;
+            m_line.Activated = true;
+        }
+
+        public SpecialTickStatus Tick()
+        {
+            if (m_switchDelayTics > 0)
+            {
+                m_switchDelayTics--;
+                return SpecialTickStatus.Continue;
+            }
+
+            m_manager.SetLineSwitch(m_line);
+
+            if (m_repeat)
+            {
+                m_switchDelayTics = 35;
+                m_repeat = false;
+                return SpecialTickStatus.Continue;
+            }
+
+            if (m_line.Flags.Repeat)
+                m_line.Activated = false;
+
+            return SpecialTickStatus.Destroy;
+        }
+
+        public void Use()
+        {
+        }
+    }
+}
