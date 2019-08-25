f31965d79acd5adf1810621f75c6bc42edfd1723
Chris <38708476+wchrisk@users.noreply.github.com>
2019-08-25 19:29:14 -0400

    Added font FontDefinition tests

---
+++ Tests/Unit/Resources/Definitions/Fonts/Definition/FontDefinitionTest.cs
@@ -0,0 +1,18 @@
+using Helion.Resources.Definitions.Fonts.Definition;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Helion.Test.Unit.Resources.Definitions.Fonts.Definition
+{
+    [TestClass]
+    public class FontDefinitionTest
+    {
+        [TestMethod]
+        public void CreateFontDefinition()
+        {
+            const string name = "name";
+            FontDefinition fontDefinition = new FontDefinition(name);
+            
+            Assert.AreEqual(name, fontDefinition.Name);
+        }
+    }
+}
