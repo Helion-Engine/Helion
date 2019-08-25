c04014d63e9cfd3ca4990a2206c5d253e5df119d
Chris <38708476+wchrisk@users.noreply.github.com>
2019-08-25 19:26:13 -0400

    Added font CharDefinition test

---
+++ Tests/Unit/Resources/Definitions/Fonts/Definition/CharDefinitionTest.cs
@@ -0,0 +1,28 @@
+using Helion.Resources.Definitions.Fonts.Definition;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Helion.Test.Unit.Resources.Definitions.Fonts.Definition
+{
+    [TestClass]
+    public class CharDefinitionTest
+    {
+        [TestMethod]
+        public void CanConstructCharDefinition()
+        {
+            CharDefinition definition = new CharDefinition('c', "img", true, FontAlignment.Top);
+            
+            Assert.AreEqual('c', definition.Character);
+            Assert.AreEqual("img", definition.ImageName);
+            Assert.IsTrue(definition.Default);
+            Assert.AreEqual(FontAlignment.Top, definition.Alignment);
+        }
+        
+        [TestMethod]
+        public void ConstructorWithNullWorks()
+        {
+            CharDefinition definition = new CharDefinition('c', "img", true, null);
+
+            Assert.IsNull(definition.Alignment);
+        }
+    }
+}
