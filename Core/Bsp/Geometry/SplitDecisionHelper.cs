ad4b1c7c1dc71b04dc959a4b4f68ad51725d339c
Chris <38708476+wchrisk@users.noreply.github.com>
2019-09-18 00:00:28 -0400

    Left a final note for myself that future work is to be done in the split finder optimization

--- Core/Bsp/Geometry/SplitDecisionHelper.cs
+++ Core/Bsp/Geometry/SplitDecisionHelper.cs
@@ -1,17 +1,14 @@
 using System.Collections.Generic;
 using Helion.Maps;
 using Helion.Maps.Components;
-using NLog;
 using static Helion.Util.Assertion.Assert;
 
 namespace Helion.Bsp.Geometry
 {
     public class SplitDecisionHelper
     {
-        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
-
         private readonly IMap m_map;
-        private bool m_corrupt;
+        private bool m_nodesTooInaccurateToUse;
         
         public SplitDecisionHelper(IMap map)
         {
@@ -22,7 +19,7 @@ namespace Helion.Bsp.Geometry
         {
             node = null;
             
-            if (m_corrupt || m_map.GetNodes().Count == 0)
+            if (m_nodesTooInaccurateToUse || m_map.GetNodes().Count == 0)
                 return null;
 
             if (nodeIndex >= m_map.GetNodes().Count)
@@ -37,11 +34,16 @@ namespace Helion.Bsp.Geometry
                 if (!segment.IsMiniseg && node.Splitter.Collinear(segment))
                     return segment;
 
-            if (!m_corrupt)
-            {
-                m_corrupt = true;
-                Log.Warn("Nodes built for the map are malformed, will have to discover node splitters the slow way");
-            }
+            // TODO: More work needs to be done on this. Unfortunately vanilla
+            // has an issue with slime trails due to imprecisions and we might
+            // not be able to account for this imprecision safely. More work
+            // needs to be done and the splitter should be drawn in the BSP
+            // visualizer to see what is going on here.
+            //
+            // Since we get a bit of a performance boost on maps where it does
+            // work, we'll leave this in here for now.
+            if (!m_nodesTooInaccurateToUse)
+                m_nodesTooInaccurateToUse = true;
             
             return null;
         }
