ad1b28e29fbb2653cee80e19839bcd6138a7a447
Chris <38708476+wchrisk@users.noreply.github.com>
2019-09-25 10:29:13 -0400

    Started work on a CollinearOverlapResolver (to be done later though)

--- Core/Bsp/Repairer/MapBlock.cs
+++ Core/Bsp/Repairer/MapBlock.cs
@@ -6,5 +6,6 @@ namespace Helion.Bsp.Repairer
     public class MapBlock
     {
         public readonly List<BspSegment> Segments = new List<BspSegment>();
+        public readonly List<BspSegment> OneSidedSegments = new List<BspSegment>();
     }
 }
--- Core/Bsp/Repairer/MapRepairer.cs
+++ Core/Bsp/Repairer/MapRepairer.cs
@@ -4,16 +4,18 @@ using Helion.Bsp.Geometry;
 using Helion.Util.Extensions;
 using Helion.Util.Geometry;
 using Helion.Util.Geometry.Boxes;
+using NLog;
 
 namespace Helion.Bsp.Repairer
 {
     public class MapRepairer
     {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
         private readonly List<BspSegment> m_segments;
         private readonly VertexAllocator m_vertexAllocator;
         private readonly SegmentAllocator m_segmentAllocator;
         private readonly UniformGrid<MapBlock> m_anySegmentBlocks;
-        private readonly UniformGrid<MapBlock> m_oneSidedSegmentBlocks;
 
         public MapRepairer(List<BspSegment> segments, VertexAllocator vertexAllocator, SegmentAllocator segmentAllocator)
         {
@@ -23,7 +25,6 @@ namespace Helion.Bsp.Repairer
 
             Box2D bounds = vertexAllocator.Bounds();
             m_anySegmentBlocks = new UniformGrid<MapBlock>(bounds);
-            m_oneSidedSegmentBlocks = new UniformGrid<MapBlock>(bounds);
             AddSegmentsToBlocks();
         }
 
@@ -34,15 +35,37 @@ namespace Helion.Bsp.Repairer
             repairer.PerformRepair();
             return repairer.m_segments;
         }
+        
+        private static bool HasNoOverlappingTime(double startTime, double endTime)
+        {
+            const double epsilon = 0.0001;
+            const double endEpsilon = 1.0 - epsilon;
+
+            // Note that this may or may not do what we want for very long or
+            // very short lines. With very long lines, an intersection time
+            // can be small but actually have some distance. Likewise, a small
+            // time for a short line might be a rounding error.
+            //
+            // I've seen the same vertices get mapped to +/- 0.0000000000000002
+            // or some really small number, so I'm hoping that using something
+            // like 0.0001 for the epsilon will magically work for all cases.
+            // If we ever get any classification issues, then we'll be forced
+            // to accept the segment that is in the [0, 1] range and calculate
+            // the distance it goes into the segment and go from there.
+            bool beforeStart = startTime <= epsilon && endTime <= epsilon;
+            bool afterEnd = startTime >= endEpsilon && endTime >= endEpsilon;
+            return beforeStart || afterEnd;
+        }
 
         private void AddSegmentsToBlocks()
         {
             foreach (BspSegment segment in m_segments)
             {
-                m_anySegmentBlocks.Iterate(segment, block => block.Segments.Add(segment));
-                
-                if (segment.OneSided)
-                    m_oneSidedSegmentBlocks.Iterate(segment, block => block.Segments.Add(segment));
+                m_anySegmentBlocks.Iterate(segment, block =>
+                { 
+                    block.Segments.Add(segment);
+                    block.OneSidedSegments.Add(segment);
+                });
             }
         }
 
@@ -67,26 +90,27 @@ namespace Helion.Bsp.Repairer
                 
                 foreach (MapBlock block in m_anySegmentBlocks)
                 {
-                    foreach ((BspSegment first, BspSegment second) in block.Segments.PairCombinations())
+                    foreach ((BspSegment firstSeg, BspSegment secondSeg) in block.Segments.PairCombinations())
                     {
-                        if (!first.Collinear(second))
+                        if (!firstSeg.Collinear(secondSeg))
                             continue;
                         
-                        // 1) Take an arbitrary line, then find the times for all the
-                        // vertices on that line, and sort them by that.
-                        // TODO
+                        double startTime = firstSeg.ToTime(secondSeg.Start);
+                        double endTime = firstSeg.ToTime(secondSeg.End);
+                        if (HasNoOverlappingTime(startTime, endTime))
+                            continue;
 
-                        // 2) Every vertex that has 2+ lines coming out of it will be
-                        // preserved and merged.
-                        // TODO
-
-                        // 3) Endpoints are preserved always
-                        // TODO
-
-                        // 4) Anything in the middle that is not (2) will be thrown out.
-                        // TODO
+                        Log.Error("Found overlapping segments for line {0} ({1} -> {2}) and {3} ({4} -> {5})", 
+                            firstSeg.Line?.Id, firstSeg.Start, firstSeg.End, secondSeg.Line?.Id, secondSeg.Start, secondSeg.End);
+                        
+                        var overlapResolver = new CollinearOverlapResolver(firstSeg, secondSeg, startTime, endTime);
+                        overlapResolver.Resolve();
+                        // TODO: Do things with the resolved segment data.
+                        // TODO: Add seg to be removed to the segsToRemove.
                     }
                 }
+                
+                // TODO: Remove any segments in segsToRemove from the SegmentAllocator.
             } 
             while (segsToRemove.Count > 0);
         }
