54b39176b1be68f571fd68785f5fa799c83c14f4
Chris <38708476+wchrisk@users.noreply.github.com>
2019-11-02 19:09:43 -0400

    Allow travis to use the helper script

--- travis-helper.sh
+++ travis-helper.sh
@@ -2,6 +2,7 @@
 
 # We want to copy Rules.ruleset into all of the directories with a .csproj, at
 # least until we figure out why it's not being respected on Linux w/ Travis...
+echo "Running TravisCI helper script"
 for D in *; do
     if [ -d "${D}" ]; then
 	count=`ls -l ${D}/*.csproj 2>/dev/null | wc -l`
