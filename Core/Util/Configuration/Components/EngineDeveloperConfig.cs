1ab82efcaa5090a2091f05bbcb8a25ef9f5488c7
Chris <38708476+WChrisK@users.noreply.github.com>
2020-07-08 15:47:36 -0400

    Add cvar for using zdbsp over the internal nodebuilder (default to zdbsp)

--- Core/Util/Configuration/Components/EngineDeveloperConfig.cs
+++ Core/Util/Configuration/Components/EngineDeveloperConfig.cs
@@ -9,5 +9,6 @@ namespace Helion.Util.Configuration.Components
         public readonly ConfigValue<bool> MouseFocus = new ConfigValue<bool>(true);
         public readonly ConfigValue<bool> RenderDebug = new ConfigValue<bool>(false);
         public readonly ConfigValue<bool> RemoveHitEntity = new ConfigValue<bool>(true);
+        public readonly ConfigValue<bool> UseZdbsp = new ConfigValue<bool>(true);
     }
 }
