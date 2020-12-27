7c69813a0eee8960bdb03bee4c301ab437009180
Chris <38708476+WChrisK@users.noreply.github.com>
2020-12-27 18:44:53 -0500

    Started font overhaul (WIP)

--- Core/Graphics/Fonts/Font.cs
+++ Core/Graphics/Fonts/Font.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using Helion.Util.Extensions;
+using Helion.Util.Geometry.Boxes;
+using Helion.Util.Geometry.Vectors;
 using static Helion.Util.Assertion.Assert;
 
 namespace Helion.Graphics.Fonts
@@ -8,66 +12,108 @@ namespace Helion.Graphics.Fonts
     /// <summary>
     /// A collection of a set of glyphs for some font.
     /// </summary>
-    public class Font : IEnumerable<Glyph>
+    public class Font : IEnumerable<FontGlyph>
     {
+        /// <summary>
+        /// The character to use when a letter cannot be found.
+        /// </summary>
+        public const char DefaultChar = '?';
+
         /// <summary>
         /// The metrics for the entire font.
         /// </summary>
         public readonly FontMetrics Metrics;
 
         /// <summary>
-        /// The default glyph for this font.
+        /// The image that contains all the compiled glyphs. They can be looked
+        /// up with the character glyph for their location in this.
         /// </summary>
-        public readonly Glyph DefaultGlyph;
-        
-        private readonly Dictionary<char, Glyph> m_glyphs = new Dictionary<char, Glyph>();
+        public readonly Image Image;
 
-        /// <summary>
-        /// Gets how many glyphs are present.
-        /// </summary>
-        public int Count => m_glyphs.Count;
-        
+        private readonly Dictionary<char, FontGlyph> m_glyphs = new();
+
         /// <summary>
         /// Creates a new font from a series of glyphs.
         /// </summary>
-        /// <param name="defaultGlyph">The default glyph.</param>
         /// <param name="glyphs">A list of all the glyphs. This must contain
         /// the default glyph.</param>
         /// <param name="metrics">The font metrics for drawing with.</param>
-        public Font(Glyph defaultGlyph, List<Glyph> glyphs, FontMetrics metrics)
+        public Font(List<Glyph> glyphs, FontMetrics metrics)
         {
             Precondition(!glyphs.Empty(), "Cannot make a font that has no glyphs");
-            
-            DefaultGlyph = defaultGlyph;
-            m_glyphs[defaultGlyph.Character] = defaultGlyph;
-            glyphs.ForEach(g => m_glyphs[g.Character] = g);
+
             Metrics = metrics;
+
+            Rectangle imageArea = default;
+            if (glyphs.Empty())
+                GenerateMissingGlyph(ref imageArea);
+            else
+            {
+                imageArea = CalculateImageArea(glyphs);
+                PopulateGlyphs(glyphs, imageArea);
+            }
+
+            Image = CreateImage(imageArea);
         }
 
-        /// <summary>
-        /// Gets the glyph for the byte. If it does not have a glyph mapping
-        /// then a default value is returned.
-        /// </summary>
-        /// <param name="b">The byte.</param>
-        /// <returns>The glyph for the byte.</returns>
-        public Glyph this[byte b] => m_glyphs.GetValueOrDefault((char)b, DefaultGlyph);
+        private void GenerateMissingGlyph(ref Rectangle imageArea)
+        {
+            Image image = ImageHelper.CreateNullImage();
+            imageArea = new Rectangle(0, 0, image.Width, image.Height);
+            Box2D uv = new(Vec2D.Zero, Vec2D.One);
 
-        /// <summary>
-        /// Gets the glyph for the char. If it does not have a glyph mapping
-        /// then a default value is returned.
-        /// </summary>
-        /// <param name="c">The char.</param>
-        /// <returns>The glyph for the char.</returns>
-        public Glyph this[char c] => m_glyphs.GetValueOrDefault(c, DefaultGlyph);
+            m_glyphs[DefaultChar] = new FontGlyph(DefaultChar, image, imageArea, uv);
+        }
 
-        /// <summary>
-        /// Checks if the font has the character as a glyph.
-        /// </summary>
-        /// <param name="c">The character to check.</param>
-        /// <returns>True if so, false otherwise.</returns>
-        public bool HasCharacter(char c) => m_glyphs.ContainsKey(c);
+        private static Rectangle CalculateImageArea(IEnumerable<Glyph> glyphs)
+        {
+            int width = 0;
+            int height = 0;
 
-        public IEnumerator<Glyph> GetEnumerator() => m_glyphs.Values.GetEnumerator();
+            foreach (Glyph glyph in glyphs)
+            {
+                width += glyph.Image.Width;
+                height = Math.Max(height, glyph.Image.Height);
+            }
+
+            return new Rectangle(0, 0, width, height);
+        }
+
+        private void PopulateGlyphs(IEnumerable<Glyph> glyphs, Rectangle imageArea)
+        {
+            int offsetX = 0;
+            Vec2D uvFactor = new Vec2D(1.0 / imageArea.Width, 1.0 / imageArea.Height);
+
+            foreach (Glyph glyph in glyphs)
+            {
+                char c = glyph.Character;
+                Image image = glyph.Image;
+
+                Rectangle location = new Rectangle(offsetX, 0, image.Width, image.Height);
+                Vec2D topLeftUV = new Vec2D(offsetX, 0) * uvFactor;
+                Vec2D bottomRightUV = new Vec2D(offsetX + image.Width, image.Height) * uvFactor;
+                Box2D uv = new Box2D(topLeftUV, bottomRightUV);
+
+                m_glyphs[c] = new FontGlyph(c, image, location, uv);
+
+                offsetX += glyph.Image.Width;
+            }
+        }
+
+        private Image CreateImage(Rectangle imageArea)
+        {
+            Image image = new(imageArea.Width, imageArea.Height, Color.Transparent);
+
+            foreach (FontGlyph glyph in m_glyphs.Values)
+            {
+                Vec2I drawAt = new(glyph.Location.X, glyph.Location.Y);
+                glyph.Image.DrawOnTopOf(image, drawAt);
+            }
+
+            return image;
+        }
+
+        public IEnumerator<FontGlyph> GetEnumerator() => m_glyphs.Values.GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
---
+++ Core/Graphics/Fonts/FontGlyph.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using Helion.Util.Geometry.Boxes;
+
+namespace Helion.Graphics.Fonts
+{
+    /// <summary>
+    /// A glyph for a font. This is the atomic element in a font.
+    /// </summary>
+    public record FontGlyph
+    {
+        /// <summary>
+        /// The letter this glyph represents.
+        /// </summary>
+        public readonly char Letter;
+
+        /// <summary>
+        /// The image that makes up this glyph.
+        /// </summary>
+        public readonly Image Image;
+
+        /// <summary>
+        /// The location in the larger image of a font. Used primarily for
+        /// rendering. The origin is at the top left of the image.
+        /// </summary>
+        public readonly Rectangle Location;
+
+        /// <summary>
+        /// The UV coordinates. Intended for rendering. The top left is (0, 0)
+        /// and the bottom right is (1, 1), unlike the OpenGL's convention.
+        /// </summary>
+        public readonly Box2D UV;
+
+        public FontGlyph(char letter, Image image, Rectangle location, Box2D uv)
+        {
+            Letter = letter;
+            Image = image;
+            Location = location;
+            UV = uv;
+        }
+    }
+}
