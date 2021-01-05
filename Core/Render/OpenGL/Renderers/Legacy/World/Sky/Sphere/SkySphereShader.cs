b12e4ad9f69e8c8fdc91e3a39c8aad50c6f5cde0
Chris <38708476+WChrisK@users.noreply.github.com>
2021-01-04 19:52:45 -0500

    Invulnerability sky added

--- Core/Render/OpenGL/Renderers/Legacy/World/Sky/Sphere/SkySphereShader.cs
+++ Core/Render/OpenGL/Renderers/Legacy/World/Sky/Sphere/SkySphereShader.cs
@@ -9,10 +9,11 @@ namespace Helion.Render.OpenGL.Renderers.Legacy.World.Sky.Sphere
     public class SkySphereShader : ShaderProgram
     {
         public readonly UniformInt BoundTexture = new UniformInt();
+        public readonly UniformInt HasInvulnerability = new UniformInt();
         public readonly UniformMatrix4 Mvp = new UniformMatrix4();
         public readonly UniformFloat ScaleU = new UniformFloat();
 
-        public SkySphereShader(IGLFunctions functions, ShaderBuilder builder, VertexArrayAttributes attributes) : 
+        public SkySphereShader(IGLFunctions functions, ShaderBuilder builder, VertexArrayAttributes attributes) :
             base(functions, builder, attributes)
         {
         }
@@ -35,7 +36,7 @@ namespace Helion.Render.OpenGL.Renderers.Legacy.World.Sky.Sphere
                     gl_Position = mvp * vec4(pos, 1.0);
                 }
             ";
-            
+
             const string fragmentShaderText = @"
                 #version 130
 
@@ -45,12 +46,22 @@ namespace Helion.Render.OpenGL.Renderers.Legacy.World.Sky.Sphere
 
                 uniform float scaleU;
                 uniform sampler2D boundTexture;
+                uniform int hasInvulnerability;
 
                 void main() {
                     fragColor = texture(boundTexture, vec2(uvFrag.x * scaleU, uvFrag.y));
+
+                    // If invulnerable, grayscale everything and crank the brightness.
+                    // Note: The 1.5x is a visual guess to make it look closer to vanilla.
+                    if (hasInvulnerability != 0)
+                    {
+                        float maxColor = max(max(fragColor.x, fragColor.y), fragColor.z);
+                        maxColor *= 1.5;
+                        fragColor.xyz = vec3(maxColor, maxColor, maxColor);
+                    }
                 }
             ";
-            
+
             VertexShaderComponent vertexShaderComponent = new VertexShaderComponent(functions, vertexShaderText);
             FragmentShaderComponent fragmentShaderComponent = new FragmentShaderComponent(functions, fragmentShaderText);
             return new ShaderBuilder(vertexShaderComponent, fragmentShaderComponent);
