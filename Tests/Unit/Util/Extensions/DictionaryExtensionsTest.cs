9629bb78be5e9a7bc580dd93097a1cdbdf95bf7f
Chris <38708476+WChrisK@users.noreply.github.com>
2021-03-13 20:17:44 -0500

    Added dictionary tests

---
+++ Tests/Unit/Util/Extensions/DictionaryExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Helion.Util.Extensions;
+using Xunit;
+
+namespace Helion.Tests.Unit.Util.Extensions
+{
+    public class DictionaryExtensionsTest
+    {
+        [Fact(DisplayName = "Check if a dictionary is empty")]
+        public void CheckEmptyDictionary()
+        {
+            Dictionary<int, string> dictionary = new();
+            dictionary.Empty().Should().BeTrue();
+
+            dictionary[1] = "hi";
+            dictionary.Empty().Should().BeFalse();
+        }
+    }
+}
