03d43fe4fb24f19c4fbb8a67513f0c285c7b5985
Chris <38708476+WChrisK@users.noreply.github.com>
2021-03-14 07:10:05 -0400

    Added list tests

---
+++ Tests/Unit/Util/Extensions/ListExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Helion.Util.Extensions;
+using Xunit;
+
+namespace Helion.Tests.Unit.Util.Extensions
+{
+    public class ListExtensionsTest
+    {
+        [Fact(DisplayName = "Check if a list is empty")]
+        public void CheckEmptyLinkedList()
+        {
+            List<int> list = new();
+            list.Empty().Should().BeTrue();
+
+            list.Add(1);
+            list.Empty().Should().BeFalse();
+        }
+    }
+}
