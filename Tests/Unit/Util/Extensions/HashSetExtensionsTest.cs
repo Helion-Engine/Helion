879b7a703432ebece83afa71b02b57642ca83084
Chris <38708476+WChrisK@users.noreply.github.com>
2021-03-13 20:23:35 -0500

    Added HashSet and IEnumerable tests

---
+++ Tests/Unit/Util/Extensions/HashSetExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Helion.Util.Extensions;
+using Xunit;
+
+namespace Helion.Tests.Unit.Util.Extensions
+{
+    public class HashSetExtensions
+    {
+        [Fact(DisplayName = "Check if a hash set is empty")]
+        public void CheckEmptySet()
+        {
+            HashSet<int> set = new();
+            set.Empty().Should().BeTrue();
+
+            set.Add(1);
+            set.Empty().Should().BeFalse();
+        }
+    }
+}
---
+++ Tests/Unit/Util/Extensions/IEnumerableExtensionsTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FluentAssertions;
+using Helion.Util.Extensions;
+using Xunit;
+
+namespace Helion.Tests.Unit.Util.Extensions
+{
+    public class IEnumerableExtensions
+    {
+        [Fact(DisplayName = "Check if an enumerable set is empty")]
+        public void CheckEmptyIEnumerable()
+        {
+            SomeEnumerable e = new(null);
+            e.Empty().Should().BeTrue();
+        }
+        
+        [Fact(DisplayName = "Check if a non-empty enumerable is empty")]
+        public void CheckNonEmptyIEnumerable()
+        {
+            SomeEnumerable e = new(1);
+            e.Empty().Should().BeFalse();
+        }
+
+        private class SomeEnumerable : IEnumerable<int>
+        {
+            private readonly int? m_value;
+
+            public SomeEnumerable(int? value)
+            {
+                m_value = value;
+            }
+            
+            public IEnumerator<int> GetEnumerator()
+            {
+                if (m_value != null)
+                    yield return m_value.Value;
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
