270d716cec66322d1b9828dd77e6cf8092d24a83
Nick <stlaurent.nicholas@gmail.com>
2021-04-29 07:08:47 -0400

    implement checking for current/default language

--- Core/Resources/Definitions/Language/LanguageDefinition.cs
+++ Core/Resources/Definitions/Language/LanguageDefinition.cs
@@ -2,43 +2,70 @@
 using Helion.World.Entities.Players;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Helion.Resources.Definitions.Language
 {
     public class LanguageDefinition
     {
+        public CultureInfo CultureInfo { get; set; } = CultureInfo.CurrentCulture;
+
         private readonly Dictionary<string, string> m_lookup = new(StringComparer.OrdinalIgnoreCase);
 
         public void Parse(string data)
         {
+            data = GetCurrentLanguageSection(data);
+
             SimpleParser parser = new SimpleParser();
             parser.Parse(data);
             StringBuilder sb = new StringBuilder();
 
             while (!parser.IsDone())
             {
-                if (parser.ConsumeIf("["))
-                {
-                    string language = parser.ConsumeString();
-                    bool isDefault = parser.ConsumeIf("default");
-                    parser.ConsumeString("]");
-                    continue;
-                }
-
                 string key = parser.ConsumeString();
                 parser.ConsumeString("=");
 
                 sb.Clear();
                 do
                 {
-                    sb.Append(parser.ConsumeString());
+                    sb.Append(parser.ConsumeString().Replace("\\n", "\n"));
                 } while (!parser.Peek(';'));
 
                 parser.ConsumeString(";");
 
                 m_lookup.Add(key, sb.ToString());
             }
+        }
+
+        private string GetCurrentLanguageSection(string data)
+        {
+            Regex currentLanguage = new Regex(string.Format("\\[{0}\\w?(\\s+default)?]", CultureInfo.TwoLetterISOLanguageName));
+            Regex defaultLanguage = new Regex("\\[\\w+\\s+default]");
+            Regex anyLanguage = new Regex("\\[\\w+(\\s+default)?]");
+
+            Match m = currentLanguage.Match(data);
+            if (m.Success)
+                return GetLanguageSection();
+
+            m = defaultLanguage.Match(data);
+            if (m.Success)
+                return GetLanguageSection();
+
+            string GetLanguageSection()
+            {
+                int startIndex = m.Index + m.Length;
+                int endIndex = data.Length;
+                m = anyLanguage.Match(data, startIndex);
+
+                if (m.Success)
+                    endIndex = m.Index;
+
+                return data[startIndex..endIndex];
+            }
+
+            return data;
         }
 
         public string[] GetMessages(string message)
