8a341711742c8f313d05b7238513bf1a9a8ef0c4
Chris <38708476+WChrisK@users.noreply.github.com>
2021-06-05 16:09:57 -0400

    Added documentation

--- Core/Render/OpenGL/Renderers/World/IGLWorldRenderer.cs
+++ Core/Render/OpenGL/Renderers/World/IGLWorldRenderer.cs
@@ -4,9 +4,23 @@ using Helion.World;
 
 namespace Helion.Render.OpenGL.Renderers.World
 {
+    /// <summary>
+    /// The interface for a class that handles specific world rendering parts.
+    /// This is not meant to include primitives like lines, planes, etc, things
+    /// best suited for primitive 3D renderers.
+    /// </summary>
     public interface IGLWorldRenderer : IDisposable
     {
+        /// <summary>
+        /// Draws the world
+        /// </summary>
+        /// <param name="world">The world to draw.</param>
         void Draw(IWorld world);
+        
+        /// <summary>
+        /// Performs rendering after <see cref="Draw"/> has been invoked.
+        /// </summary>
+        /// <param name="context">The rendering context information.</param>
         void Render(WorldRenderContext context);
     }
 }
