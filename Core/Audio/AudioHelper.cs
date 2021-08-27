bfefa14280fca69f0b8de3f40d3b7d85928a23a5
Nick <stlaurent.nicholas@gmail.com>
2021-08-27 15:31:16 -0400

    implement playing wave files, update dssecret to higher bitrate wave file

--- Core/Audio/AudioHelper.cs
+++ Core/Audio/AudioHelper.cs
@@ -1,4 +1,6 @@
+using NAudio.Wave;
 using System;
+using System.IO;
 
 namespace Helion.Audio
 {
@@ -9,9 +11,33 @@ namespace Helion.Audio
         private const int SampleRateArbitraryCutoff = 5000;
         private const int DataBeforeSamplesSize = 2 + 2 + 4 + DmxPadding;
         private const int MinRequiredDmxLength = DataBeforeSamplesSize + DmxPadding;
+        private const int WavBufferSize = 16384;
 
-        public static bool TryReadDoomSound(byte[] data, out int sampleRate, out Span<byte> sampleData)
+        public struct WavFormat
         {
+            public int Channels { get; set; }
+            public int SampleRate { get; set; }
+            public int BitsPerSample { get; set; }
+        };
+
+        public static bool IsWave(byte[] data) =>
+            data.Length >= 4 && data[0] == 'R' && data[1] == 'I' && data[2] == 'F' && data[3] == 'F';
+
+        public static bool IsDoomSound(byte[] data)
+        {
+            if (data.Length < 8)
+                return false;
+
+            int offset = 0;
+            if (ReadUInt16(data, ref offset) != 3)
+                return false;
+
+            return true;
+        }
+
+        public static bool TryReadDoomSound(byte[] data, out int sampleRate, out Span<byte> sampleData, out string? error)
+        {
+            error = "Invalid Doom sound.";
             sampleRate = 0;
             sampleData = Array.Empty<byte>();
             
@@ -19,7 +45,7 @@ namespace Helion.Audio
                 return false;
 
             int offset = 0;
-            if (ReadUInt16(data, ref offset)  != DmxHeader)
+            if (ReadUInt16(data, ref offset) != DmxHeader)
                 return false;
 
             sampleRate = ReadUInt16(data, ref offset);
@@ -32,6 +58,42 @@ namespace Helion.Audio
 
             sampleData = new Span<byte>(data, offset, numSamples);
             return true;
+        }
+
+        public static bool TryReadWav(byte[] data, out WavFormat format, out Span<byte> sampleData)
+        {
+            format = default;
+
+            try
+            {
+                using WaveFileReader wave = new WaveFileReader(new MemoryStream(data));
+                using MemoryStream writeStream = new MemoryStream();
+                using WaveFileWriter writer = new WaveFileWriter(writeStream, wave.WaveFormat);
+
+                byte[] buffer = new byte[WavBufferSize];
+                int readAmount = buffer.Length - (buffer.Length % wave.WaveFormat.BlockAlign);
+                int length = wave.Read(buffer, 0, readAmount);
+                while (length > 0)
+                {
+                    writer.Write(buffer, 0, length);
+                    length = wave.Read(buffer, 0, readAmount);
+                }
+
+                int mod = (int)writeStream.Length % WavBufferSize;
+                if (mod != 0)
+                    writeStream.Write(new byte[mod], 0, mod);
+
+                format.Channels = wave.WaveFormat.Channels;
+                format.SampleRate = wave.WaveFormat.SampleRate;
+                format.BitsPerSample = wave.WaveFormat.BitsPerSample;
+                sampleData = new Span<byte>(writeStream.ToArray());
+                return true;
+            }
+            catch
+            {
+                sampleData = Array.Empty<byte>();
+                return false;
+            }
         }
 
         private static ushort ReadUInt16(byte[] data, ref int offset)
