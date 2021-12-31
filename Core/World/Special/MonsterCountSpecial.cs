e274ef330765a482c542735162da49c1115345e2
Nick <stlaurent.nicholas@gmail.com>
2021-12-31 06:27:20 -0500

    fix check, should not check for death state finished

--- Core/World/Special/MonsterCountSpecial.cs
+++ Core/World/Special/MonsterCountSpecial.cs
@@ -29,7 +29,7 @@ class MonsterCountSpecial : ISpecial
 
     public SpecialTickStatus Tick()
     {
-        if (m_world.EntityAliveCount(EntityEditorId, true) == 0)
+        if (m_world.EntityAliveCount(EntityEditorId, false) == 0)
         {
             ExecuteSpecial();
             return SpecialTickStatus.Destroy;
