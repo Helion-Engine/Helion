2fc0d999e5cc8e1b2eb4b686a5ec92cb7fed38d6
Chris <38708476+WChrisK@users.noreply.github.com>
2022-12-16 15:33:16 -0800

    Added GL time querying support

---
+++ Core/Render/OpenGL/Queries/GLElapsedTimeQuery.cs
@@ -0,0 +1,60 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Diagnostics;
+
+namespace Helion.Render.OpenGL.Queries;
+
+public class GLElapsedTimeQuery : GLQuery
+{
+    public GLElapsedTimeQuery(string label) : base(label)
+    {
+    }
+
+    // Times the function and returns how many milliseconds it takes.
+    public static double Time(Action func, string label = "Anonymous elapsed query timer")
+    {
+        using GLElapsedTimeQuery query = new(label);
+
+        query.Start();
+        func();
+        query.Stop();
+
+        while (query.IsDone())
+        {
+            // Spin until it's done.
+        }
+
+        return query.DurationInMilliseconds();
+    }
+
+    public void Start()
+    {
+        GL.BeginQuery(QueryTarget.TimeElapsed, Name);
+    }
+
+    public void Stop()
+    {
+        GL.EndQuery(QueryTarget.TimeElapsed);
+    }
+
+    public bool IsDone()
+    {
+        GL.GetQueryObject(Name, GetQueryObjectParam.QueryResultAvailable, out int result);
+        return result != 0;
+    }
+
+    public long DurationInNanoseconds()
+    {
+        Debug.Assert(IsDone(), $"Trying to get query value but the query is not available yet");
+
+        GL.GetQueryObject(Name, GetQueryObjectParam.QueryResult, out long duration);
+        return duration;
+    }
+
+    public double DurationInMilliseconds()
+    {
+        const double NanoToMilliseconds = 1000000.0;
+
+        return DurationInNanoseconds() / NanoToMilliseconds;
+    }
+}
---
+++ Core/Render/OpenGL/Queries/GLQuery.cs
@@ -0,0 +1,46 @@
+﻿using Helion;
+using Helion.Render;
+using Helion.Render.OpenGL;
+using Helion.Render.OpenGL.Queries;
+using Helion.Render.OpenGL.Util;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Reflection.Emit;
+
+namespace Helion.Render.OpenGL.Queries;
+
+public class GLQuery : IDisposable
+{
+    protected readonly int Name;
+    private readonly string m_label;
+    private bool m_disposed;
+
+    public GLQuery(string label)
+    {
+        Name = GL.GenQuery();
+        m_label = label;
+
+        GLHelper.ObjectLabel(ObjectLabelIdentifier.Query, Name, $"Query: {m_label}");
+    }
+
+    ~GLQuery()
+    {
+        Dispose(false);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (m_disposed)
+            return;
+
+        GL.DeleteQuery(Name);
+
+        m_disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
