f2906356270b3500ea2553bf3f18f52aedfc2d8c
Nick <stlaurent.nicholas@gmail.com>
2023-05-11 07:27:56 -0400

    centerview when turning off mouse.look

--- Client/Client.Config.cs
+++ Client/Client.Config.cs
@@ -1,3 +1,5 @@
+using Helion.World.Entities.Players;
+
 namespace Helion.Client;
 
 public partial class Client
@@ -6,6 +8,15 @@ public partial class Client
     {
         m_config.Audio.MusicVolume.OnChanged += MusicVolume_OnChanged;
         m_config.Audio.SoundVolume.OnChanged += SoundVolume_OnChanged;
+        m_config.Mouse.Look.OnChanged += Look_OnChanged;
+    }
+
+    private void Look_OnChanged(object? sender, bool set)
+    {
+        if (m_layerManager.WorldLayer == null || set)
+            return;
+
+        m_layerManager.WorldLayer.AddCommand(TickCommands.CenterView);
     }
 
     private void UnregisterConfigChanges()
