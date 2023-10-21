6e8a4af50d2206e43652724fb690452ccd2a25c0
Chris <38708476+WChrisK@users.noreply.github.com>
2023-10-20 19:46:24 -0700

    Added last few options

--- Core/Util/Configs/Components/ConfigCompat.cs
+++ Core/Util/Configs/Components/ConfigCompat.cs
@@ -1,3 +1,4 @@
+using Helion.Util.Configs.Options;
 using Helion.Util.Configs.Values;
 
 namespace Helion.Util.Configs.Components;
@@ -5,26 +6,34 @@ namespace Helion.Util.Configs.Components;
 public class ConfigCompat
 {
     [ConfigInfo("Vanilla method for finding shortest texture. Emulates bug with AASHITTY.", save: false, serialize: true, demo: true)]
+    [OptionMenu(OptionSectionType.Compatibility, "Find shortest texture")]
     public readonly ConfigValue<bool> VanillaShortestTexture = new(true);
 
     [ConfigInfo("If dehacked should be preferred over decorate in the same archive.", demo: true)]
+    [OptionMenu(OptionSectionType.Compatibility, "Use dehacked over decorate")]
     public readonly ConfigValue<bool> PreferDehacked = new(true);
 
     [ConfigInfo("Use vanilla sector physics. Floors can move through ceiling. Only one move special per sector at a time.", serialize: true, demo: true)]
+    [OptionMenu(OptionSectionType.Compatibility, "Vanilla sector physics")]
     public readonly ConfigValue<bool> VanillaSectorPhysics = new(false);
 
     [ConfigInfo("Allow items to dropoff tall ledges.", serialize: true, demo: true)]
+    [OptionMenu(OptionSectionType.Compatibility, "Items drop off ledges")]
     public readonly ConfigValue<bool> AllowItemDropoff = new(true);
 
     [ConfigInfo("Emulate vanilla infinitely tall things.", serialize: true, demo: true)]
+    [OptionMenu(OptionSectionType.Compatibility, "Infinitely tall things")]
     public readonly ConfigValue<bool> InfinitelyTallThings = new(false);
 
     [ConfigInfo("Things use their original vanilla heights for projectile collision checks.", serialize: true, demo: true)]
+    [OptionMenu(OptionSectionType.Compatibility, "Vanilla projectile height collision checks")]
     public readonly ConfigValue<bool> MissileClip = new(false);
 
     [ConfigInfo("Limits lost souls spawn by pain elementals to 21.", serialize: true, demo: true)]
+    [OptionMenu(OptionSectionType.Compatibility, "Limit pain elemental lost souls to 21")]
     public readonly ConfigValue<bool> PainElementalLostSoulLimit = new(false);
 
     [ConfigInfo("Disables item drop tossing.", serialize: true, demo: true)]
+    [OptionMenu(OptionSectionType.Compatibility, "Disable item drop tossing")]
     public readonly ConfigValue<bool> NoTossDrops = new(false);
 }
--- Core/Util/Configs/Components/ConfigRender.cs
+++ Core/Util/Configs/Components/ConfigRender.cs
@@ -1,4 +1,5 @@
 using Helion.Render.Common.Textures;
+using Helion.Util.Configs.Options;
 using Helion.Util.Configs.Values;
 using static Helion.Util.Configs.Values.ConfigFilters;
 
@@ -14,26 +15,32 @@ public enum RenderVsyncMode
 public class ConfigRenderFilter
 {
     [ConfigInfo("The kind of filter applied to fonts.")]
+    [OptionMenu(OptionSectionType.Render, "Font filtering")]
     public readonly ConfigValue<FilterType> Font = new(FilterType.Nearest, OnlyValidEnums<FilterType>());
 
     [ConfigInfo("The filter to be applied to textures.")]
+    [OptionMenu(OptionSectionType.Render, "Texture filtering")]
     public readonly ConfigValue<FilterType> Texture = new(FilterType.Nearest, OnlyValidEnums<FilterType>());
 }
 
 public class ConfigRender
 {
     [ConfigInfo("The anisotropic filtering amount. A value of 1 is the same as being off.")]
+    [OptionMenu(OptionSectionType.Render, "Anisotropy")]
     public readonly ConfigValue<int> Anisotropy = new(8, GreaterOrEqual(1));
 
     [ConfigInfo("Emulate fake contrast like vanilla Doom.")]
+    [OptionMenu(OptionSectionType.Render, "Emulate vanilla contract")]
     public readonly ConfigValue<bool> FakeContrast = new(true);
 
     public readonly ConfigRenderFilter Filter = new();
 
     [ConfigInfo("If true, forces the pipeline to be flushed after rendering a frame. May fix a laggy buffered feeling on lower end computers.")]
+    [OptionMenu(OptionSectionType.Render, "Force pipeline flush (for old GPUs)")]
     public readonly ConfigValue<bool> ForcePipelineFlush = new(false);
 
     [ConfigInfo("A cap on the maximum amount of frames per second. Zero is equivalent to no cap.")]
+    [OptionMenu(OptionSectionType.Render, "Max FPS")]
     public readonly ConfigValue<int> MaxFPS = new(250, fps =>
     {
         return fps switch
@@ -45,44 +52,58 @@ public class ConfigRender
     });
 
     [ConfigInfo("The multisampling amount. A value of 1 is the same as being off.")]
+    [OptionMenu(OptionSectionType.Render, "Multisampling", disabled: true)]
     public readonly ConfigValue<int> Multisample = new(1, GreaterOrEqual(1));
 
     [ConfigInfo("If any sprite should clip the floor.")]
+    [OptionMenu(OptionSectionType.Render, "Sprite floor clip")]
     public readonly ConfigValue<bool> SpriteClip = new(true);
 
     [ConfigInfo("Max percentage of height allowed to clip the floor for corpses.")]
+    [OptionMenu(OptionSectionType.Render, "Sprite clip max height percentage")]
     public readonly ConfigValue<double> SpriteClipFactorMax = new(0.02, ClampNormalized);
 
     [ConfigInfo("The minimum sprite height to allow to clip the floor.")]
+    [OptionMenu(OptionSectionType.Render, "Sprite clip min height")]
     public readonly ConfigValue<int> SpriteClipMin = new(16, GreaterOrEqual(0));
 
     [ConfigInfo("Checks if sprites will overlap and z-fight.")]
+    [OptionMenu(OptionSectionType.Render, "Sprite Z-fighting check")]
     public readonly ConfigValue<bool> SpriteZCheck = new(true);
 
     [ConfigInfo("If VSync should be on or off. Prevents tearing, but affects input processing (unless you have g-sync).")]
+    [OptionMenu(OptionSectionType.Render, "VSync")]
     public readonly ConfigValue<RenderVsyncMode> VSync = new(RenderVsyncMode.On);
 
     [ConfigInfo("Adds to the rendering light level offset.")]
+    [OptionMenu(OptionSectionType.Render, "Extra lighting")]
     public readonly ConfigValue<int> ExtraLight = new(0);
 
     [ConfigInfo("Draws everything at full brightness.")]
+    [OptionMenu(OptionSectionType.Render, "Full brightness")]
     public readonly ConfigValue<bool> Fullbright = new(false);
 
     [ConfigInfo("Enable sprite transparency.")]
+    [OptionMenu(OptionSectionType.Render, "Sprite transparency")]
     public readonly ConfigValue<bool> SpriteTransparency = new(true);
 
     [ConfigInfo("Enable texture transparency.")]
+    [OptionMenu(OptionSectionType.Render, "Texture transparency")]
     public readonly ConfigValue<bool> TextureTransparency = new(true);
 
     [ConfigInfo("Max render distance.")]
+    [OptionMenu(OptionSectionType.Render, "Max rendering distance")]
     public readonly ConfigValue<int> MaxDistance = new(0);
 
     [ConfigInfo("Traverses the BSP in a separate thread to mark lines seen for automap. Ignored if using BSP rendering.")]
+    [OptionMenu(OptionSectionType.Render, "Automap on separate thread")]
     public readonly ConfigValue<bool> AutomapBspThread = new(true);
 
     [ConfigInfo("Field of view. Default = 90")]
+    [OptionMenu(OptionSectionType.Render, "Field of view")]
     public readonly ConfigValue<double> FieldOfView = new(90, Clamp(60.0, 120.0));
 
     [ConfigInfo("Enable rendering missing textures as a red/black checkered texture.", mapRestartRequired: true)]
+    [OptionMenu(OptionSectionType.Render, "Render null textures as checkered")]
     public readonly ConfigValue<bool> NullTexture = new(false);
 }
