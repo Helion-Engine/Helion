cca02c1bd87b2603eac778b0c88a4a76126937b2
Chris <38708476+WChrisK@users.noreply.github.com>
2023-10-25 13:25:00 -0700

    Set the mouse position in the mock test to adhere to the interface

--- Tests/Unit/GameLayer/MockWindow.cs
+++ Tests/Unit/GameLayer/MockWindow.cs
@@ -2,11 +2,13 @@
 using Helion.Render;
 using Helion.Window;
 using System;
+using Helion.Geometry.Vectors;
 
 namespace Helion.Tests.Unit.GameLayer;
 
 public class MockWindow : IWindow
 {
+    public Vec2I MousePosition { get; private set; }
     private readonly IInputManager m_inputManager;
 
     public MockWindow(IInputManager inputManager)
@@ -21,9 +23,14 @@ public class MockWindow : IWindow
     public Dimension Dimension => new(0, 0);
 
     public Dimension FramebufferDimension => new(0, 0);
+    
+    public void SetMousePosition(Vec2I pos)
+    {
+        MousePosition = pos;
+    }
 
     public void Dispose()
     {
-
+        // Does nothing.
     }
 }
