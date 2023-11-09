1688530242155137872de2f18c34e0dbbdf4b726
Nick <stlaurent.nicholas@gmail.com>
2023-11-09 14:08:28 -0500

    add player options to general

--- Core/Util/Configs/Components/ConfigPlayer.cs
+++ Core/Util/Configs/Components/ConfigPlayer.cs
@@ -1,3 +1,4 @@
+using Helion.Util.Configs.Options;
 using Helion.Util.Configs.Values;
 using Helion.World.Entities.Players;
 using static Helion.Util.Configs.Values.ConfigFilters;
@@ -7,8 +8,10 @@ namespace Helion.Util.Configs.Components;
 public class ConfigPlayer
 {
     [ConfigInfo("The name of the player.")]
+    [OptionMenu(OptionSectionType.General, "Player Name", spacer: true)]
     public readonly ConfigValue<string> Name = new("Player", IfEmptyDefaultTo("Player"));
 
     [ConfigInfo("The gender of the player.")]
+    [OptionMenu(OptionSectionType.General, "Player Gender")]
     public readonly ConfigValue<PlayerGender> Gender = new(default, OnlyValidEnums<PlayerGender>());
 }
