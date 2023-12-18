aa1eeecf0c33702ca584df1d4b66449e4996ad62
Nick <stlaurent.nicholas@gmail.com>
2023-12-18 06:50:44 -0500

    add generic floor transfer special trigger test and ceiling/floor crush tests

--- Tests/Unit/GameAction/Boom/BoomActions_GenericPlane.cs
+++ Tests/Unit/GameAction/Boom/BoomActions_GenericPlane.cs
@@ -10,7 +10,7 @@ public partial class BoomActions
     const string DefaultFloor = "RROCK19";
     const string DefaultCeiling = "SLIME16";
 
-    [Fact(DisplayName = "Generic floor to next floor + transfer special")]
+    [Fact(DisplayName = "Generic floor to next floor + transfer special numeric")]
     public void GenericFloorToNextFloorTransferSpecial()
     {
         var sector = GameActions.GetSectorByTag(World, 29);
@@ -31,7 +31,7 @@ public partial class BoomActions
         sector.Floor.Z.Should().Be(16);
     }
 
-    [Fact(DisplayName = "Generic floor to lowest ceiling + transfer special")]
+    [Fact(DisplayName = "Generic floor to lowest ceiling + transfer special numeric")]
     public void GenericFloorToLowestCeilingTransferSpecial()
     {
         var sector = GameActions.GetSectorByTag(World, 30);
@@ -52,7 +52,7 @@ public partial class BoomActions
         sector.Floor.Z.Should().Be(48);
     }
 
-    [Fact(DisplayName = "Generic ceiling to lowest ceiling + transfer special")]
+    [Fact(DisplayName = "Generic ceiling to lowest ceiling + transfer special numeric")]
     public void GenericCeilingToLowestCeilingTransferSpecial()
     {
         var sector = GameActions.GetSectorByTag(World, 31);
@@ -72,7 +72,7 @@ public partial class BoomActions
         sector.Ceiling.Z.Should().Be(64);
     }
 
-    [Fact(DisplayName = "Generic ceiling to highest floor + transfer special")]
+    [Fact(DisplayName = "Generic ceiling to highest floor + transfer special numeric")]
     public void GenericCeilingToHighestFloorTransferSpecial()
     {
         var sector = GameActions.GetSectorByTag(World, 32);
@@ -90,5 +90,61 @@ public partial class BoomActions
         sector.KillEffect.Should().Be(InstantKillEffect.KillUnprotectedPlayer);
         GameActions.CheckPlaneTexture(World, sector.Ceiling, "SLIME13");
         sector.Ceiling.Z.Should().Be(64);
+    }
+
+    [Fact(DisplayName = "Generic floor lower 32 + transfer special trigger")]
+    public void GenericFloorTransferSpecialTrigger()
+    {
+        var sector = GameActions.GetSectorByTag(World, 33);
+        sector.SectorDamageSpecial.Should().BeNull();
+        GameActions.CheckPlaneTexture(World, sector.Floor, DefaultFloor);
+        GameActions.ActivateLine(World, Player, 519, ActivationContext.UseLine).Should().BeTrue();
+
+        GameActions.TickWorld(World, 1);
+        sector.SectorDamageSpecial.Should().BeNull();
+        GameActions.CheckPlaneTexture(World, sector.Floor, DefaultFloor);
+
+        GameActions.RunSectorPlaneSpecial(World, sector);
+        GameActions.CheckPlaneTexture(World, sector.Floor, "RROCK01");
+        sector.SectorDamageSpecial.Should().NotBeNull();
+        sector.Floor.Z.Should().Be(8);
+    }
+
+    [Fact(DisplayName = "Generic floor crusher")]
+    public void GenericFloorCrusher()
+    {
+        var sector = GameActions.GetSectorByTag(World, 34);
+        var monster = GameActions.GetSectorEntity(World, sector.Id, "DoomImp");
+        monster.Health.Should().Be(60);
+        sector.Floor.Z.Should().Be(0);
+        GameActions.ActivateLine(World, Player, 527, ActivationContext.UseLine).Should().BeTrue();
+
+        GameActions.TickWorld(World, 1);
+        // Floor crush doesn't continue when blocked but still damages
+        monster.Health.Should().BeLessThan(60);
+        sector.Floor.Z.Should().Be(0);
+
+        GameActions.RunSectorPlaneSpecial(World, sector);
+        monster.Health.Should().Be(0);
+        sector.Floor.Z.Should().Be(56);
+    }
+
+    [Fact(DisplayName = "Generic ceiling crusher")]
+    public void GenericCeilingCrusher()
+    {
+        var sector = GameActions.GetSectorByTag(World, 35);
+        var monster = GameActions.GetSectorEntity(World, sector.Id, "DoomImp");
+        monster.Health.Should().Be(60);
+        sector.Ceiling.Z.Should().Be(56);
+        GameActions.ActivateLine(World, Player, 552, ActivationContext.UseLine).Should().BeTrue();
+
+        GameActions.TickWorld(World, 1);
+        // Ceiling crush doesn't block and does crush damage like vanilla doom
+        monster.Health.Should().BeLessThan(60);
+        sector.Ceiling.Z.Should().Be(55);
+
+        GameActions.RunSectorPlaneSpecial(World, sector);
+        monster.Health.Should().Be(0);
+        sector.Ceiling.Z.Should().Be(0);
     }
 }
