f1110664b2b5dc68a4941230fd5adc221e808225
Nick <stlaurent.nicholas@gmail.com>
2024-01-04 13:36:25 -0500

    add hitscan flag tests

---
+++ Tests/Unit/GameAction/Physics/Physics_Hitscan.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Helion.Geometry.Vectors;
+using Xunit;
+
+namespace Helion.Tests.Unit.GameAction;
+
+public partial class Physics
+{
+    private Vec3D HitscanEnemyPos = new(1920, 896, 0);
+    private Vec3D HitscanPlayerPos = new(1920, 832, 0);
+
+    [Fact(DisplayName = "Hitscan")]
+    public void Hitscan()
+    {
+        var entity = GameActions.CreateEntity(World, "Zombieman", HitscanEnemyPos);
+        GameActions.SetEntityPosition(World, Player, HitscanPlayerPos);
+        Player.AngleRadians = GameActions.GetAngle(Bearing.North);
+        var hitEntity = World.FireHitscan(Player, Player.AngleRadians, 0, 8192, 0);
+        hitEntity.Should().Be(entity, "Shootable and solid");
+
+        entity.Flags.Solid = false;
+        hitEntity = World.FireHitscan(Player, Player.AngleRadians, 0, 8192, 0);
+        hitEntity.Should().Be(entity, "Shootable and non-solid");
+
+        entity.Flags.Shootable = false;
+        hitEntity = World.FireHitscan(Player, Player.AngleRadians, 0, 8192, 0);
+        hitEntity.Should().BeNull("Non-shootable and non-solid");
+
+        entity.Flags.Solid = false;
+        hitEntity = World.FireHitscan(Player, Player.AngleRadians, 0, 8192, 0);
+        hitEntity.Should().BeNull("Non-shootable and solid");
+    }
+}
