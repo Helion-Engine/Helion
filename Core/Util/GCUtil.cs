fa4895bd02949965b2c1fe9dba081edfb4d5f6ae
Nick <stlaurent.nicholas@gmail.com>
2024-05-01 05:49:40 -0400

    lower collection count so leaving menu doesn't create a long pause

--- Core/Util/GCUtil.cs
+++ Core/Util/GCUtil.cs
@@ -4,9 +4,9 @@ namespace Helion.Util;
 
 public static class GCUtil
 {
-    public static void ForceGarbageCollection()
+    public static void ForceGarbageCollection(int forceCount = 1)
     {
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < forceCount; i++)
         {
             GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true);
             GC.WaitForPendingFinalizers();
