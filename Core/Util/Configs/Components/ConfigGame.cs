d8d7fe5a45f0a1cf717d0ee509fb2fa116ac6d91
Nick <stlaurent.nicholas@gmail.com>
2024-05-24 18:34:09 -0400

    serialize no monsters and fast monsters

--- Core/Util/Configs/Components/ConfigGame.cs
+++ Core/Util/Configs/Components/ConfigGame.cs
@@ -32,7 +32,7 @@ public class ConfigGame
     [OptionMenu(OptionSectionType.General, "Confirm quick save")]
     public readonly ConfigValue<bool> QuickSaveConfirm = new(true);
 
-    [ConfigInfo("Enables fast monsters.", save: false, demo: true)]
+    [ConfigInfo("Enables fast monsters.", save: false, demo: true, serialize: true)]
     [OptionMenu(OptionSectionType.General, "Fast monsters", spacer: true)]
     public readonly ConfigValue<bool> FastMonsters = new(false);
 
@@ -46,7 +46,7 @@ public class ConfigGame
     [ConfigInfo("If stats should be written to levelstat.txt.", save: false)]
     public readonly ConfigValue<bool> LevelStat = new(false);
 
-    [ConfigInfo("Whether no monsters should be spawned.", save: false)]
+    [ConfigInfo("Whether no monsters should be spawned.", save: false, serialize: true)]
     public readonly ConfigValue<bool> NoMonsters = new(false);
 
     [ConfigInfo("Marks lines and secctors that are activated by a special in the automap.")]
