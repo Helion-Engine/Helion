d5f6a0097407aa1f5c03a98ab1c26192cdbca4e3
lemming104 <chrisaelwell@gmail.com>
2024-08-26 10:39:46 -0700

    Option section defaults feature 1.  Include default value in description text at bottom of screen 2.  Highlight non-default config values in menu 3.  Provide option to reset all config values in a section to defaults

--- Core/Util/Configs/Options/OptionMenuAttribute.cs
+++ Core/Util/Configs/Options/OptionMenuAttribute.cs
@@ -5,16 +5,18 @@ namespace Helion.Util.Configs.Options;
 [AttributeUsage(AttributeTargets.Field)]
 public class OptionMenuAttribute : Attribute
 {
-    public OptionMenuAttribute(OptionSectionType section, string name, bool disabled = false, bool spacer = false)
+    public OptionMenuAttribute(OptionSectionType section, string name, bool disabled = false, bool spacer = false, bool allowReset = true)
     {
         Section = section;
         Name = name;
         Disabled = disabled;
         Spacer = spacer;
+        AllowReset = allowReset;
     }
 
     public readonly OptionSectionType Section;
     public readonly string Name;
     public readonly bool Disabled;
     public readonly bool Spacer;
+    public readonly bool AllowReset;
 }
