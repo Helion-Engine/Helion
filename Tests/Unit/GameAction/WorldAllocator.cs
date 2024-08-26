7327b175eb919e70df9691d70d5986de6ae0dcc1
Nick <stlaurent.nicholas@gmail.com>
2024-08-26 14:02:59 -0400

    check automap bsp thread config so unit tests do not create it, count world test run time for fun

--- Tests/Unit/GameAction/WorldAllocator.cs
+++ Tests/Unit/GameAction/WorldAllocator.cs
@@ -3,7 +3,6 @@ using Helion.Audio.Impl;
 using Helion.Layer.Worlds;
 using Helion.Maps.Shared;
 using Helion.Models;
-using Helion.Resources;
 using Helion.Resources.Archives.Collection;
 using Helion.Resources.Archives.Locator;
 using Helion.Resources.IWad;
@@ -20,89 +19,95 @@ using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.IO.Compression;
 using Helion.Maps.Bsp.Zdbsp;
+using NLog;
 
-namespace Helion.Tests.Unit.GameAction
+namespace Helion.Tests.Unit.GameAction;
+
+internal static class WorldAllocator
 {
-    internal static class WorldAllocator
+    public static int TotalTicks;
+    private static SinglePlayerWorld? StaticWorld = null;
+    private static string LastResource = string.Empty;
+    private static string LastFileName = string.Empty;
+    private static string LastMapName = string.Empty;
+    private static string LastTestKey = string.Empty;
+
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+    public static SinglePlayerWorld LoadMap(string resourceZip, string fileName, string mapName, string testKey, Action<SinglePlayerWorld> onInit,
+        IWadType iwadType = IWadType.Doom2, SkillLevel skillLevel = SkillLevel.Medium, Player? existingPlayer = null, WorldModel? worldModel = null, 
+        bool disposeExistingWorld = true, bool cacheWorld = true,
+        Action<ArchiveCollection>? onBeforeInit = null)
     {
-        private static SinglePlayerWorld? StaticWorld = null;
-        private static string LastResource = string.Empty;
-        private static string LastFileName = string.Empty;
-        private static string LastMapName = string.Empty;
-        private static string LastTestKey = string.Empty;
+        if (disposeExistingWorld && UseExistingWorld(resourceZip, fileName, mapName, testKey, cacheWorld, out SinglePlayerWorld? existingWorld))
+            return existingWorld;
 
-        public static SinglePlayerWorld LoadMap(string resourceZip, string fileName, string mapName, string testKey, Action<SinglePlayerWorld> onInit,
-            IWadType iwadType = IWadType.Doom2, SkillLevel skillLevel = SkillLevel.Medium, Player? existingPlayer = null, WorldModel? worldModel = null, 
-            bool disposeExistingWorld = true, bool cacheWorld = true,
-            Action<ArchiveCollection>? onBeforeInit = null)
-        {
-            if (disposeExistingWorld && UseExistingWorld(resourceZip, fileName, mapName, testKey, cacheWorld, out SinglePlayerWorld? existingWorld))
-                return existingWorld;
+        // Assets.pk3 is copied from the assets project.
+        File.Exists(Constants.AssetsFileName).Should().BeTrue();
 
-            // Assets.pk3 is copied from the assets project.
-            File.Exists(Constants.AssetsFileName).Should().BeTrue();
+        LastResource = resourceZip;
+        LastFileName = fileName;
+        LastMapName = mapName;
+        LastTestKey = testKey;
 
-            LastResource = resourceZip;
-            LastFileName = fileName;
-            LastMapName = mapName;
-            LastTestKey = testKey;
+        using ZipArchive archive = ZipFile.OpenRead(resourceZip);
+        archive.ExtractToDirectory(Directory.GetCurrentDirectory(), true);
+        Config config = CreateConfig();
+        var profiler = new Profiler();
+        var audioSystem = new MockAudioSystem();
+        ArchiveCollection archiveCollection = new(new FilesystemArchiveLocator(), config, new DataCache());
+        archiveCollection.Load([fileName], iwad: null, iwadTypeOverride: iwadType).Should().BeTrue();
 
-            using ZipArchive archive = ZipFile.OpenRead(resourceZip);
-            archive.ExtractToDirectory(Directory.GetCurrentDirectory(), true);
-            Config config = CreateConfig();
-            var profiler = new Profiler();
-            var audioSystem = new MockAudioSystem();
-            ArchiveCollection archiveCollection = new(new FilesystemArchiveLocator(), config, new DataCache());
-            archiveCollection.Load(new string[] { fileName }, iwad: null, iwadTypeOverride: iwadType).Should().BeTrue();
+        var mapDef = archiveCollection.Definitions.MapInfoDefinition.MapInfo.GetMapInfoOrDefault(mapName);
+        var skillDef = archiveCollection.Definitions.MapInfoDefinition.MapInfo.GetSkill(skillLevel) ?? throw new Exception("Failed to load skill definition");
+        var map = archiveCollection.FindMap(mapDef.MapName) ?? throw new Exception("Failed to load map");
+        Zdbsp zdbsp = new();
+        if (!zdbsp.RunZdbsp(map, mapName, mapDef, out var outputMap) || outputMap == null)
+            throw new Exception("Failed to create bsp");
 
-            var mapDef = archiveCollection.Definitions.MapInfoDefinition.MapInfo.GetMapInfoOrDefault(mapName);
-            var skillDef = archiveCollection.Definitions.MapInfoDefinition.MapInfo.GetSkill(skillLevel);
-            if (skillDef == null)
-                throw new Exception("Failed to load skill definition");
+        onBeforeInit?.Invoke(archiveCollection);
 
-            var map = archiveCollection.FindMap(mapDef.MapName);
-            if (map == null)
-                throw new Exception("Failed to load map");
+        DoomRandom random = worldModel == null ? new() : new(worldModel.RandomIndex);
+        SinglePlayerWorld? world = WorldLayer.CreateWorldGeometry(new GlobalData(), config, audioSystem, archiveCollection, profiler, mapDef, 
+            skillDef, outputMap, existingPlayer, worldModel, random, unitTest: true) ?? throw new Exception("Failed to create world");
+        StaticWorld = world;
+        world.OnTick += World_OnTick;
+        world.Start(worldModel);
+        world.OnDestroying += World_OnDestroying;
+        onInit(world);
+        return world;
+    }
 
-            Zdbsp zdbsp = new();
-            if (!zdbsp.RunZdbsp(map, mapName, mapDef, out var outputMap) || outputMap == null)
-                throw new Exception("Failed to create bsp");
+    private static void World_OnDestroying(object? sender, EventArgs e)
+    {
+        Log.Info("Total world test time: {time}", TimeSpan.FromSeconds(TotalTicks / Constants.TicksPerSecond));
+    }
 
-            onBeforeInit?.Invoke(archiveCollection);
+    private static void World_OnTick(object? sender, EventArgs e)
+    {
+        TotalTicks++;
+    }
 
-            DoomRandom random = worldModel == null ? new() : new(worldModel.RandomIndex);
-            SinglePlayerWorld? world = WorldLayer.CreateWorldGeometry(new GlobalData(), config, audioSystem, archiveCollection, profiler, mapDef, 
-                skillDef, outputMap, existingPlayer, worldModel, random, unitTest: true);
-            if (world == null)
-                throw new Exception("Failed to create world");
+    private static Config CreateConfig()
+    {
+        Config config = new();
+        config.Render.AutomapBspThread.Set(false);
+        return config;
+    }
 
-            StaticWorld = world;
-            world.Start(worldModel);
-            onInit(world);
-            return world;
-        }
-
-        private static Config CreateConfig()
-        {
-            Config config = new();
-            config.Render.AutomapBspThread.Set(false);
-            return config;
-        }
-
-        private static bool UseExistingWorld(string resourceZip, string fileName, string mapName, string testKey, bool cacheWorld,
-            [NotNullWhen(true)] out SinglePlayerWorld? existingWorld)
-        {
-            existingWorld = StaticWorld;
-            if (StaticWorld == null)
-                return false;
-
-            if (cacheWorld && existingWorld != null && resourceZip.EqualsIgnoreCase(LastResource) && fileName.EqualsIgnoreCase(LastFileName) && 
-                mapName.EqualsIgnoreCase(LastMapName) && testKey.EqualsIgnoreCase(LastTestKey))
-                return true;
-
-            StaticWorld.ArchiveCollection.Dispose();
-            StaticWorld.Dispose();
+    private static bool UseExistingWorld(string resourceZip, string fileName, string mapName, string testKey, bool cacheWorld,
+        [NotNullWhen(true)] out SinglePlayerWorld? existingWorld)
+    {
+        existingWorld = StaticWorld;
+        if (StaticWorld == null)
             return false;
-        }
+
+        if (cacheWorld && existingWorld != null && resourceZip.EqualsIgnoreCase(LastResource) && fileName.EqualsIgnoreCase(LastFileName) && 
+            mapName.EqualsIgnoreCase(LastMapName) && testKey.EqualsIgnoreCase(LastTestKey))
+            return true;
+
+        StaticWorld.ArchiveCollection.Dispose();
+        StaticWorld.Dispose();
+        return false;
     }
 }
