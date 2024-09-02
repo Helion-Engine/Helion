c3de65a0f19b0f41e374f4ce444377169b17ceec
lemming104 <chrisaelwell@gmail.com>
2024-09-01 22:29:37 -0700

    Make cursor keys skip over subheaders in Options sections

--- Core/Layer/Options/Sections/ListedConfigSection.cs
+++ Core/Layer/Options/Sections/ListedConfigSection.cs
@@ -479,10 +479,16 @@ public class ListedConfigSection : IOptionSection
 
     private void AdvanceToValidRow(int direction)
     {
-        m_currentRowIndex += direction;
-        if (m_currentRowIndex < 0)
-            m_currentRowIndex += m_configValues.Count + 1;
-        m_currentRowIndex %= m_configValues.Count + 1;
+        bool ranOnce = false;
+
+        while (ranOnce == false || (m_currentRowIndex < m_configValues.Count && m_configValues[m_currentRowIndex].Attr.Disabled))
+        {
+            m_currentRowIndex += direction;
+            if (m_currentRowIndex < 0)
+                m_currentRowIndex += m_configValues.Count + 1;
+            m_currentRowIndex %= m_configValues.Count + 1;
+            ranOnce = true;
+        }
 
         m_soundManager.PlayStaticSound(MenuSounds.Cursor);
     }
