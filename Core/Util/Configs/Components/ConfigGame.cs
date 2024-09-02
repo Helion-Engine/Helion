e8f38274208c0cf3c49c463e6cc03960e79ca949
lemming104 <50272946+lemming104@users.noreply.github.com>
2024-09-02 09:50:28 -0700

    Reorganize options menu to group items more logically (#667)
    * Reorganize General options, add headers
    
    * Reorganize Hud and Render menus
    
    * Make cursor keys skip over subheaders in Options sections
    
    * Remove section headers but keep organization
    
    * Revert unneeded change

--- Core/Util/Configs/Components/ConfigGame.cs
+++ Core/Util/Configs/Components/ConfigGame.cs
@@ -8,6 +8,8 @@ namespace Helion.Util.Configs.Components;
 
 public class ConfigGame
 {
+    // Controls/input
+
     [ConfigInfo("Always run.", demo: true)]
     [OptionMenu(OptionSectionType.General, "Always Run")]
     public readonly ConfigValue<bool> AlwaysRun = new(true);
@@ -20,23 +22,29 @@ public class ConfigGame
     [OptionMenu(OptionSectionType.General, "Horizontal Autoaim")]
     public readonly ConfigValue<bool> HorizontalAutoAim = new(false);
 
+    [ConfigInfo("Player can use lines (doors, switches) by bumping into them.", demo: true)]
+    [OptionMenu(OptionSectionType.General, "Bump Use")]
+    public readonly ConfigValue<bool> BumpUse = new(false);
+
+
+    // Visual effects
+
     [ConfigInfo("Scale red amount drawn to screen when the player takes damage.")]
-    [OptionMenu(OptionSectionType.General, "Pain Intensity")]
+    [OptionMenu(OptionSectionType.General, "Pain Intensity", spacer: true)]
     public readonly ConfigValue<double> PainIntensity = new(1.0);
 
     [ConfigInfo("Transition effect between levels/screens.")]
     [OptionMenu(OptionSectionType.General, "Transition Type")]
     public readonly ConfigValue<World.TransitionType> TransitionType = new(World.TransitionType.Melt);
 
-    [ConfigInfo("Player can use lines (doors, switches) by bumping into them.", demo: true)]
-    [OptionMenu(OptionSectionType.General, "Bump Use", spacer: true)]
-    public readonly ConfigValue<bool> BumpUse = new(false);
+
+    // Saving and loading
 
     [ConfigInfo("Attempt to load latest saved game on death.")]
-    [OptionMenu(OptionSectionType.General, "Load Latest on Death")]
+    [OptionMenu(OptionSectionType.General, "Load Latest on Death", spacer: true)]
     public readonly ConfigValue<bool> LoadLatestOnDeath = new(true);
 
-    [ConfigInfo("Automatically save at level start.", save: false)]
+    [ConfigInfo("Automatically save at level start.", save: true)]
     [OptionMenu(OptionSectionType.General, "Autosave")]
     public readonly ConfigValue<bool> AutoSave = new(false);
 
@@ -44,25 +52,8 @@ public class ConfigGame
     [OptionMenu(OptionSectionType.General, "Confirm Quick Save")]
     public readonly ConfigValue<bool> QuickSaveConfirm = new(true);
 
-    [ConfigInfo("Enable fast monsters.", save: false, demo: true, serialize: true)]
-    [OptionMenu(OptionSectionType.General, "Fast Monsters", spacer: true)]
-    public readonly ConfigValue<bool> FastMonsters = new(false);
 
-    [ConfigInfo("Enable monster closet detection and limited monster AI.", mapRestartRequired: true, demo: true)]
-    [OptionMenu(OptionSectionType.General, "Monster Closet Detection")]
-    public readonly ConfigValue<bool> MonsterCloset = new(true);
-
-    [ConfigInfo("Skill level to use when starting a map.", save: false, demo: true)]
-    public readonly ConfigValue<SkillLevel> Skill = new(SkillLevel.Medium, ConfigSetFlags.OnNewWorld, OnlyValidEnums<SkillLevel>());
-
-    [ConfigInfo("Write stats to levelstat.txt.", save: false)]
-    public readonly ConfigValue<bool> LevelStat = new(false);
-
-    [ConfigInfo("Whether no monsters should be spawned.", save: false, serialize: true)]
-    public readonly ConfigValue<bool> NoMonsters = new(false);
-
-    [ConfigInfo("Reset the player's inventory at the start of each map.", save: false, serialize: true)]
-    public readonly ConfigValue<bool> PistolStart = new(false);
+    // Cheats
 
     [ConfigInfo("Mark lines and sectors that are activated by a special in the automap.")]
     [OptionMenu(OptionSectionType.General, "Mark Specials", spacer: true)]
@@ -71,6 +62,32 @@ public class ConfigGame
     [ConfigInfo("Mark secret sectors in the automap.")]
     [OptionMenu(OptionSectionType.General, "Mark Secrets")]
     public readonly ConfigValue<bool> MarkSecrets = new(false);
+
+    
+    // Difficulty modifiers
+
+    [ConfigInfo("Remove all monsters from the game.", save: false, serialize: true)]
+    [OptionMenu(OptionSectionType.General, "No Monsters", spacer: true)]
+    public readonly ConfigValue<bool> NoMonsters = new(false);
+
+    [ConfigInfo("Reset the player's inventory at the start of each map.", save: false, serialize: true)]
+    [OptionMenu(OptionSectionType.General, "Pistol Starts")]
+    public readonly ConfigValue<bool> PistolStart = new(false);
+
+    [ConfigInfo("Enable fast monsters.", save: false, demo: true, serialize: true)]
+    [OptionMenu(OptionSectionType.General, "Fast Monsters")]
+    public readonly ConfigValue<bool> FastMonsters = new(false);
+
+
+    // Non-menu items
+    [ConfigInfo("Write stats to levelstat.txt.", save: false)]
+    public readonly ConfigValue<bool> LevelStat = new(false);
+
+    [ConfigInfo("Skill level to use when starting a map.", save: false, demo: true)]
+    public readonly ConfigValue<SkillLevel> Skill = new(SkillLevel.Medium, ConfigSetFlags.OnNewWorld, OnlyValidEnums<SkillLevel>());
+
+    [ConfigInfo("Enable monster closet detection and limited monster AI (Map restart required).", mapRestartRequired: true, demo: true)]
+    public readonly ConfigValue<bool> MonsterCloset = new(true);
 
     public SkillDef? SelectedSkillDefinition { get; set; }
 }
