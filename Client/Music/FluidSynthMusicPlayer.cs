4e6e7f8f03c2c5159205980a9d0606022bbf85d4
lemming104 <chrisaelwell@gmail.com>
2024-09-02 19:13:31 -0700

    Try using SDL2 on Linux (default seems to be ALSA)

--- Client/Music/FluidSynthMusicPlayer.cs
+++ Client/Music/FluidSynthMusicPlayer.cs
@@ -4,6 +4,7 @@ using NFluidsynth;
 using NLog;
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using static Helion.Util.Assertion.Assert;
 
 namespace Helion.Client.Music;
@@ -26,6 +27,12 @@ public class FluidSynthMusicPlayer : IMusicPlayer
     {
         m_settings = new Settings();
         m_settings[ConfigurationKeys.SynthAudioChannels].IntValue = 2;
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            m_settings[ConfigurationKeys.AudioDriver].StringValue = "sdl2";
+        }
+
         m_synth = new(m_settings);
         m_audioDriver = new(m_synth.Settings, m_synth);
         EnsureSoundFont(soundFontFile);
