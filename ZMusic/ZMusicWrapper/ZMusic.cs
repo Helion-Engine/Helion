0504474b87cb175d5a5e801cdbc75923c9d8b897
lemming104 <chrisaelwell@gmail.com>
2024-09-10 00:43:39 -0700

    Improve loading redirection in wrapper code; remove csproj file hackery

--- ZMusic/ZMusicWrapper/ZMusic.cs
+++ ZMusic/ZMusicWrapper/ZMusic.cs
@@ -23,17 +23,37 @@
             }
         }
 
+        private static string GetRuntimePath()
+        {
+#pragma warning disable IDE0046 // if/else collapsing produces very dense code here
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return Environment.Is64BitProcess
+                    ? "runtimes\\win-x64\\native\\"
+                    : "runtimes\\win-x86\\native\\";
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && Environment.Is64BitProcess)
+            {
+                return "runtimes/linux-x64/native/";
+            }
+
+            throw new NotSupportedException("This library does not support the current OS.");
+#pragma warning restore IDE0046
+        }
+
         private static string[] GetExpectedLibraryNames()
         {
 #pragma warning disable IDE0046 // if/else collapsing produces very dense code here
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                return ["zmusic.dll"];
+            {
+                return Environment.Is64BitProcess
+                    ? ["zmusic.dll", "libsndfile-1.dll", "libmpg123-0.dll"]
+                    : ["zmusic.dll", "libsndfile-1.dll", "libmpg123-0.dll"];
+            }
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                return ["libzmusic.so.1", "libzmusic.so"];
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                return ["libzmusic.dylib"];
+                return ["libzmusic.so", "libc.so.6", "libgcc_s.so.1", "libglib-2.0.so.0", "libpcre.so.3", "libstdc++.so.6"];
 
             throw new NotSupportedException("This library does not support the current machine OS.");
 #pragma warning restore IDE0046
@@ -41,21 +61,36 @@
 
         private static IntPtr ImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
         {
-            if (libraryName == ZMusic.LibraryName)
+            if (libraryName == LibraryName)
             {
+                string runtimePath = GetRuntimePath();
                 string[] libraryNames = GetExpectedLibraryNames();
 
-                foreach (string possibleName in libraryNames)
+                string primaryLibrary = libraryNames[0];
+                if (NativeLibrary.TryLoad($"{runtimePath}{primaryLibrary}", out nint handle))
                 {
-                    if (NativeLibrary.TryLoad(possibleName, out nint handle))
+                    foreach (string secondaryLibrary in libraryNames[1..])
                     {
-                        return handle;
+                        _ = NativeLibrary.TryLoad($"{runtimePath}{secondaryLibrary}", out _);
                     }
+
+                    return handle;
                 }
 
-                throw new DllNotFoundException($"Could not load a suitable substitute for DllImport {libraryName}.  Tried searching for {string.Join(',', libraryNames)}");
+                if (NativeLibrary.TryLoad(primaryLibrary, out handle))
+                {
+                    foreach (string secondaryLibrary in libraryNames[1..])
+                    {
+                        _= NativeLibrary.TryLoad($"{secondaryLibrary}", out _);
+                    }
+
+                    return handle;
+                }
+
+                throw new DllNotFoundException($"Could not load a suitable substitute for DllImport {libraryName}.");
             }
-            return NativeLibrary.Load(libraryName, assembly, searchPath);
+
+            return IntPtr.Zero;
         }
     }
 }
