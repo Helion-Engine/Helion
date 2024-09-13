3002acc0187024cf587140815c1f5dd2c484d01e
72hr <115905880+72hr@users.noreply.github.com>
2024-09-13 17:54:40 -0400

    add toggle ccmd

--- Client/Client.Commands.Toggle.cs
+++ Client/Client.Commands.Toggle.cs
@@ -1,12 +1,60 @@
-﻿using Helion.Util.Consoles.Commands;
+﻿using Helion.Util.Configs.Components;
+using Helion.Util.Configs.Values;
+using Helion.Util.Consoles.Commands;
 using Helion.Util.Consoles;
 using Helion.Util.Loggers;
 using Helion.Util;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Helion.Client;
 
 public partial class Client
 {
+    /// <summary>
+    /// Cycles a config setting's value through the provided values (optional if boolean).
+    /// If the current value is not in the list, the first one is used.
+    /// </summary>
+    [ConsoleCommand("toggle", "Toggles a config setting")]
+    private void Toggle(ConsoleCommandEventArgs args)
+    {
+        string? configKey = args.Args.FirstOrDefault();
+        if (configKey == null)
+        {
+            HelionLog.Error("Config setting not provided");
+            return;
+        }
+        if (!m_config.TryGetComponent(configKey, out ConfigComponent? component))
+        {
+            HelionLog.Error($"Config setting {configKey} not found");
+            return;
+        }
+
+        // if boolean, allow toggling without providing values
+        if (args.Args.Count == 1)
+        {
+            if (component.Value is ConfigValue<bool> boolConfigVal)
+                TryHandleConfigVariableCommand(new ConsoleCommandEventArgs($"{configKey} {!boolConfigVal.Value}"));
+            else
+                HelionLog.Error($"Must provide values for {configKey}, since it is not a True/False config setting");
+        }
+        else
+        {
+            List<object> parsedValues = [];
+            foreach (string arg in args.Args[1..])
+            {
+                if (!component.Value.TryConvert(arg, out object? parsedVal) || parsedVal == null)
+                {
+                    HelionLog.Error($"Invalid value '{arg}' provided for {configKey}");
+                    return;
+                }
+                parsedValues.Add(parsedVal);
+            }
+            int nextIndex = (parsedValues.IndexOf(component.Value.ObjectValue) + 1) % parsedValues.Count;
+            TryHandleConfigVariableCommand(new ConsoleCommandEventArgs($"{configKey} {parsedValues[nextIndex]}"));
+        }
+    }
+
     [ConsoleCommand("mouselook", "Toggle mouselook")]
     private void ToggleMouselook(ConsoleCommandEventArgs args)
     {
