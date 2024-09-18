d2dada9af27cd7f257bb41a6b6faae986da720fd
72hr <115905880+72hr@users.noreply.github.com>
2024-09-17 22:18:55 -0400

    remove commented out code

--- Core/Resources/Definitions/Compatibility/Options.cs
+++ Core/Resources/Definitions/Compatibility/Options.cs
@@ -50,71 +50,15 @@ public class Options
         val == "1"
     );
 
-    // https://doomwiki.org/wiki/OPTIONS
-    // most of these aren't supported
+    // https://doomwiki.org/wiki/OPTIONS - most are not supported
     private static bool OptionValidForCompLevel(string option, CompLevel level)
     {
-        // bool atLeastBugFixed = level >= ExecutableValues[GameConfConstants.Executable.BugFixed];
         bool atLeastBoom = level >= CompLevel.Boom;
-        // bool atLeastComplevel9 = level >= ExecutableValues[GameConfConstants.Executable.Complevel9];
-        // bool atLeastMbf = level >= CompLevel.Mbf;
-        // bool atLeastMbf21 = level >= CompLevel.Mbf21;
         return option switch
         {
-            // vanilla
-            // "comp_soul" => true,
-            // Boom
-            // "comp_blazing" when atLeastBoom => true,
-            // "comp_doorlight" when atLeastBoom => true,
-            // "comp_doorstuck" when atLeastBoom => true,
-            // "comp_floors" when atLeastBoom => true,
-            // "comp_god" when atLeastBoom => true,
-            // "comp_model" when atLeastBoom => true,
             OptionsConstants.Comp.Pain when atLeastBoom => true,
-            // "comp_skull" when atLeastBoom => true,
             OptionsConstants.Comp.Stairs when atLeastBoom => true,
             OptionsConstants.Comp.Vile when atLeastBoom => true,
-            // "comp_zerotags" when atLeastBoom => true,
-            // PrBoom
-            // "comp_zombie" when atLeastComplevel9 => true, // TODO: verify
-            // MBF
-            // "comp_dropoff" when atLeastMbf => true,
-            // "comp_falloff" when atLeastMbf => true,
-            // "comp_infcheat" when atLeastMbf => true,
-            // "comp_pursuit" when atLeastMbf => true,
-            // "comp_respawn" when atLeastMbf => true,
-            // "comp_skymap" when atLeastMbf => true,
-            // "comp_staylift" when atLeastMbf => true,
-            // "comp_telefrag" when atLeastMbf => true,
-            // "dog_jumping" when atLeastMbf => true,
-            // "friend_distance" when atLeastMbf => true,
-            // "help_friends" when atLeastMbf => true,
-            // "monkeys" when atLeastMbf => true,
-            // "monster_avoid_hazards" when atLeastMbf => true,
-            // "monster_backing" when atLeastMbf => true,
-            // "monster_friction" when atLeastMbf => true,
-            // "monster_infighting" when atLeastMbf => true,
-            // "monsters_remember" when atLeastMbf => true,
-            // "player_helpers" when atLeastMbf => true,
-            // "weapon_recoil" when atLeastMbf => true,
-            // MBF21
-            // "comp_friendlyspawn" when atLeastMbf21 => true,
-            // "comp_ledgeblock" when atLeastMbf21 => true,
-            // "comp_reservedlineflag" when atLeastMbf21 => true,
-            // "comp_voodooscroller" when atLeastMbf21 => true,
-
-            // forced off in MBF21
-            // vanilla
-            // "comp_666" when !atLeastMbf21 => true,
-            // "comp_maskedanim" when !atLeastMbf21 => true,
-            // LxDoom
-            // "comp_moveblock" when atLeastBugFixed && !atLeastMbf21 => true, // TODO: verify
-            // Boom
-            // "comp_maxhealth" when atLeastBoom && !atLeastMbf21 => true,
-            // "comp_sound" when atLeastBoom && !atLeastMbf21 => true,
-            // PrBoom+
-            // "comp_ouchface" when atLeastComplevel9 && !atLeastMbf21 => true, // TODO: verify
-
             _ => false
         };
     }
