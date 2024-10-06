129a2dd7fa8288e6bbe826734c1849462cfdf98f
lemming104 <chrisaelwell@gmail.com>
2024-10-05 17:55:18 -0700

    Clean up trim behaviors in Config

--- Core/Util/Configs/Impl/ConfigElement.cs
+++ Core/Util/Configs/Impl/ConfigElement.cs
@@ -5,16 +5,19 @@
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.Reflection;
+    using Helion.Util.Configs.Components;
+    using System;
 
 
     public interface IConfigElement
     {
-        void PopulateComponentsRecursively(List<(IConfigValue, OptionMenuAttribute, ConfigInfoAttribute)> fields, int depth);
+        void RecursivelyGetConfigFieldsOrThrow(List<(IConfigValue, OptionMenuAttribute, ConfigInfoAttribute)> fields, int depth);
+        void PopulateComponentsRecursively(Dictionary<string, ConfigComponent> components, string path, int depth);
     }
 
     public abstract class ConfigElement<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicFields)] T> : IConfigElement
     {
-        public void PopulateComponentsRecursively(List<(IConfigValue, OptionMenuAttribute, ConfigInfoAttribute)> fields, int depth = 1)
+        public void RecursivelyGetConfigFieldsOrThrow(List<(IConfigValue, OptionMenuAttribute, ConfigInfoAttribute)> fields, int depth = 1)
         {
             const int RecursiveOverflowLimit = 100;
             if (depth > RecursiveOverflowLimit)
@@ -38,7 +41,39 @@
                     continue;
                 }
 
-                (childObj as IConfigElement)?.PopulateComponentsRecursively(fields, depth + 1);
+                (childObj as IConfigElement)?.RecursivelyGetConfigFieldsOrThrow(fields, depth + 1);
+            }
+        }
+
+        public void PopulateComponentsRecursively(Dictionary<string, ConfigComponent> components, string path, int depth)
+        {
+            const int RecursiveOverflowLimit = 100;
+
+            if (depth > RecursiveOverflowLimit)
+                throw new Exception($"A public instance is missing the [ConfigComponentIgnore] attribute, possibly at: {path}");
+
+            foreach (FieldInfo fieldInfo in typeof(T).GetFields())
+            {
+                if (!fieldInfo.IsPublic)
+                    continue;
+
+                object? childObj = fieldInfo.GetValue(this);
+                if (childObj == null)
+                    throw new Exception($"Missing config object instantiation {fieldInfo.Name} at '{path}'");
+
+                string newPath = (path != "" ? $"{path}." : "") + fieldInfo.Name.ToLower();
+
+                if (childObj is IConfigValue configValue)
+                {
+                    ConfigInfoAttribute? attribute = fieldInfo.GetCustomAttribute<ConfigInfoAttribute>();
+                    if (attribute == null)
+                        throw new Exception($"Config field at '{newPath}' is missing attribute {nameof(ConfigInfoAttribute)}");
+
+                    components[newPath] = new ConfigComponent(newPath, attribute, configValue);
+                    continue;
+                }
+
+                (childObj as IConfigElement)?.PopulateComponentsRecursively(components, newPath, depth + 1);
             }
         }
     }
