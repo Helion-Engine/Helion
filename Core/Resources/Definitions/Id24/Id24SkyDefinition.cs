bf03bf3e4b2a19cdce4aeafce9891d2dc820547c
lemming104 <chrisaelwell@gmail.com>
2024-10-06 00:30:27 -0700

    Change null handling in sky defs System.Text.Json handles nulls differently than Newtonsoft.Json.

--- Core/Resources/Definitions/Id24/Id24SkyDefinition.cs
+++ Core/Resources/Definitions/Id24/Id24SkyDefinition.cs
@@ -22,13 +22,19 @@ public class Id24SkyDefinition
             var converted = (SkyDefinitions?)JsonSerializer.Deserialize(data, typeof(SkyDefinitions), SkyDefinitionSerializationContext.Default) ?? throw new Exception("Data was null");
             Data = converted.Data;
 
-            foreach (var item in Data.FlatMapping)
-                FlatMapping[item.Flat] = item.Sky;
-
-            foreach (var sky in Data.Skies)
+            if (Data.FlatMapping != null)
             {
-                if (!sky.Validate(out string error))
-                    Log.Error(error);
+                foreach (var item in Data.FlatMapping)
+                    FlatMapping[item.Flat] = item.Sky;
+            }
+
+            if (Data.Skies != null)
+            {
+                foreach (var sky in Data.Skies)
+                {
+                    if (!sky.Validate(out string error))
+                        Log.Error(error);
+                }
             }
         }
         catch (Exception ex)
