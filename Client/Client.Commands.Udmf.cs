152aaf2a04c0cd9e3fa1704f9eae3e6cb721d241
Nick <stlaurent.nicholas@gmail.com>
2025-03-10 20:14:56 -0400

    command to convert map to udmf

---
+++ Client/Client.Commands.Udmf.cs
@@ -0,0 +1,20 @@
+﻿using Helion.Util.Consoles.Commands;
+using Helion.Util.Consoles;
+using Helion.Maps.Udmf;
+
+namespace Helion.Client;
+
+public partial class Client
+{
+    [ConsoleCommand("writetextmap", "Converts a doom map to a textmap file")]
+    private void CommandWriteTextMap(ConsoleCommandEventArgs args)
+    {
+        if (args.Args.Count < 2)
+            Log.Error("Output file argument required");
+
+        if (!UdmfMapWriter.WriteMap(m_archiveCollection, args.Args[0], args.Args[1]))
+            Log.Error("Failed to write textmap");
+        else
+            Log.Info($"Successfully wrote {args.Args[1]}");
+    }
+}
