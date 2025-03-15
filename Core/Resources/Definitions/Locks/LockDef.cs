8b6ea53970950c67a831c9235c94bdcf0a22f090
Nick <stlaurent.nicholas@gmail.com>
2025-03-15 05:08:55 -0400

    add backing property for key image color
    update

--- Core/Resources/Definitions/Locks/LockDef.cs
+++ Core/Resources/Definitions/Locks/LockDef.cs
@@ -6,11 +6,26 @@ namespace Helion.Resources.Definitions.Locks;
 
 public class LockDef
 {
+    private Color? m_keyImageColor;
+
     public ZDoomKeyType KeyNumber { get; set; }
     public string DoorMessage { get; set; } = string.Empty;
     public string ObjectMessage { get; set; } = string.Empty;
     public string RemoteMessage { get; set; } = string.Empty;
     public Color MapColor { get; set; }
+    public Color KeyImageColor
+    {
+        get
+        {
+            if (m_keyImageColor.HasValue)
+                return m_keyImageColor.Value;
+            return MapColor;
+        }
+        set
+        {
+            m_keyImageColor = value;
+        }
+    }
     public List<string> KeyDefinitionNames { get; set; } = [];
     public List<List<string>> AnyKeyDefinitionNames { get; set; } = [];
 }
