9abdaf2db27c6f441d2e7d04cae5295757a05015
Nick <stlaurent.nicholas@gmail.com>
2025-05-20 05:58:34 -0400

    restore buffer cover wall to original value since it can be true for transfer heights dynamic rendering

--- Core/Render/OpenGL/Renderers/Legacy/World/Geometry/GeometryRenderer.cs
+++ Core/Render/OpenGL/Renderers/Legacy/World/Geometry/GeometryRenderer.cs
@@ -592,12 +592,13 @@ public class GeometryRenderer : IDisposable
             var visibility = GetSideVisibility(side, otherSide, facingSector, otherSector);
             if ((visibility & SideTexture.Upper) == 0 || (visibility & SideTexture.Lower) == 0)
             {
+                var bufferCoverWall = m_worldDataManager.BufferCoverWalls;
                 SetBufferCoverWall(true);
                 // Need to copy since the vertices may be part of member variable cache
                 for (int i = 0; i < midTexVertices.Length; i++) 
                     m_coverWallVertices[i] = midTexVertices[i];
                 RenderMidTexCoverWalls(side, facingSector, otherSector, m_coverWallVertices, visibility, m_renderCoverWallAction);
-                SetBufferCoverWall(false);
+                SetBufferCoverWall(bufferCoverWall);
             }
         }
     }
