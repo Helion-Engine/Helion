3131926c8e3bf63c4ba64a1d8d34269be9c5d2a1
lemming104 <50272946+lemming104@users.noreply.github.com>
2025-07-20 15:58:49 -0700

    Add script to build AppImage from Linux-AOT (#1224)
    * Write a script that builds Helion and deploys an AppImage.
    This does not ensure that all required deps (e.g. libsndfile) are present.
    
    * Minor cleanup in AppImage script
    
    * Use existing .desktop file

---
+++ Scripts/buildAndPackageAppImage.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+startdir=$PWD;
+scriptdir=$(dirname $0);
+cd $scriptdir;
+
+# TODO:  Install any other required tools and deps needed for the build
+
+# Do build
+cd ../Client
+if ! (dotnet publish -c Release -r linux-x64 -p:AOT=true); then
+    echo 'Build failed!';
+    exit 1;
+fi
+
+# Remove any existing AppImage
+rm -rf AppImage;
+rm -rf Helion.AppImage;
+
+# Copy executable and support files
+cd ..
+echo 'Copying files for AppImage';
+mkdir -p AppImage/usr/bin;
+cp -r Publish/linux-x64_AOT/* AppImage/usr/bin;
+
+# Copy all transitive dependencies
+mkdir -p AppImage/lib;
+ldd Publish/linux-x64_AOT/Helion | awk 'NF == 4 { system("cp " $3 " AppImage/lib") }';
+
+# Copy icon and .desktop file
+cp -r Scripts/appImageResources/* AppImage;
+chmod +x AppImage/AppRun;
+cp Assets/Misc/Helion.desktop AppImage;
+
+# Download AppImage tool
+if [ ! -f appimagetool-x86_64.AppImage ]; then
+    echo 'Downloading AppImage tool from GitHub';
+    wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage;
+    chmod +x appimagetool-x86_64.AppImage;
+fi
+
+ARCH=x86_64;
+if ! (./appimagetool-x86_64.AppImage AppImage Helion.AppImage); then
+    echo 'AppImage pack failed!';
+    exit 1;
+else
+    echo 'AppImage created: Helion.AppImage';
+fi
+
+# Clean up
+rm -rf AppImage;
+
+cd $startdir;
