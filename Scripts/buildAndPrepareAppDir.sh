451cdcb8d250acda30a3eaf22eb69581fbccb822
lemming <lemming104@gmail.com>
2025-08-31 16:51:41 -0700

    Add 256x256 image and .desktop file

--- Scripts/buildAndPrepareAppDir.sh
+++ Scripts/buildAndPrepareAppDir.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# This builds a generic app dir that might be a basis for distributing as a DEB or RPM, packaging into a tarball, making an AppImage, etc.
+# Some of the contents will need to be _deleted_ for some of these scenarios--for example, the contents of /usr/lib are needed for AppImage,
+# but should not be in a DEB or RPM.
+
 startdir=$PWD;
 scriptdir=$(dirname $0);
 cd $scriptdir;
@@ -28,5 +33,11 @@ ldd Publish/linux-x64_AOT/Helion | awk 'NF == 4 { system("cp " $3 " AppDir/usr/l
 rm AppDir/usr/lib/libm.so.*;
 rm AppDir/usr/lib/libc.so.*;
 patchelf --force-rpath --set-rpath '$ORIGIN'/../usr/lib AppDir/usr/bin/Helion
+
+mkdir -p AppDir/usr/share/applications
+cp Assets/Misc/Helion.desktop AppDir/usr/share/applications
+
+mkdir -p AppDir/usr/share/pixmaps
+cp Scripts/packageResources/Helion.png AppDir/usr/share/pixmaps
 
 cd $startdir;
