45a6043132c4d6930a375554f586b8f11c63f41e
Nick <stlaurent.nicholas@gmail.com>
2025-09-03 05:46:11 -0400

    swap BitConverter for direct pointer set

--- Core/Render/OpenGL/Renderers/Legacy/World/VertexOptions.cs
+++ Core/Render/OpenGL/Renderers/Legacy/World/VertexOptions.cs
@@ -6,41 +6,41 @@ namespace Helion.Render.OpenGL.Renderers.Legacy.World;
 public static class VertexOptions
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static float World(int topLeft, float alpha, int addAlpha, int upper, int lower, int lightLevelBufferIndex)
+    public static unsafe float World(int topLeft, float alpha, int addAlpha, int upper, int lower, int lightLevelBufferIndex)
     {
         int alphaByte = (int)(alpha * 255.0f);
         int packed = (alphaByte & 0xFF) | (topLeft << 8) | (addAlpha << 9) | (upper << 10) | (lower << 11) | (lightLevelBufferIndex << 12);
-        return BitConverter.Int32BitsToSingle(packed);
+        return *(float*)&packed;
     }
 
-    public static float ColorMapIndex(int colorMapIndex, int vertexLightLevel)
+    public static unsafe float ColorMapIndex(int colorMapIndex, int vertexLightLevel)
     {
         // First 8 bits are lightLevel, next 24 are colorMapIndex
         int packed = ((colorMapIndex & 0xFFFFFF) << 8) | ((vertexLightLevel) & 0xFF);
-        return BitConverter.Int32BitsToSingle(packed);
+        return *(float*)&packed;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static float LightLevelAdd(int mapId, int lightLevelAdd)
+    public static unsafe float LightLevelAdd(int mapId, int lightLevelAdd)
     {
         // Packs LightLevelAdd with MapId
         // First bit is sign, next 8 are lightLevelAdd, last 23 are mapId
         int signMask = lightLevelAdd >> 31;
         lightLevelAdd = (lightLevelAdd ^ signMask) - signMask;
         int packed = ((mapId & 0xFFFFFF) << 9) | ((lightLevelAdd & 0xFF) << 1) | (signMask & 1);
-        return BitConverter.Int32BitsToSingle(packed);
+        return *(float*)&packed;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static float Entity(float alpha, int fuzz, int flipU, int colormap, int lightLevel)
+    public static unsafe float Entity(float alpha, int fuzz, int flipU, int colormap, int lightLevel)
     {
         int alphaByte = (int)(alpha * 255.0f);
         int packed = (alphaByte & 0xFF) | (fuzz << 8) | (flipU << 9) | ((lightLevel & 0xFF) << 10) | (colormap << 18);
-        return BitConverter.Int32BitsToSingle(packed);
+        return *(float*)&packed;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static float EntityXYZ(int offsetXY, int offsetZ)
+    public static unsafe float EntityXYZ(int offsetXY, int offsetZ)
     {
         // Shift negative bit for mask to get absolute value and sign bit to remove branches
         int maskXY = offsetXY >> 31;
@@ -51,6 +51,6 @@ public static class VertexOptions
         int offsetZSign = maskZ & 1;
 
         int packed = (offsetXYSign << 31) | (offsetZSign << 30) | (offsetXY << 16) | offsetZ;
-        return BitConverter.Int32BitsToSingle(packed);
+        return *(float*)&packed;
     }
 }
