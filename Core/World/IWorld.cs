57157f9222e0d2fada9e0938dd3c2da91d02c017
Nick <42744211+nstlaurent@users.noreply.github.com>
2025-10-13 11:52:28 -0400

    remove IList abstraction and directly set lists (#1333)

--- Core/World/IWorld.cs
+++ Core/World/IWorld.cs
@@ -71,11 +71,11 @@ public interface IWorld : IDisposable
     int LevelTime { get; }
     double Gravity { get; }
     WorldState WorldState { get; }
-    IList<Line> Lines { get; }
-    IList<Side> Sides { get; }
-    IList<Sector> Sectors { get; }
+    List<Line> Lines { get; }
+    List<Side> Sides { get; }
+    List<Sector> Sectors { get; }
     DynamicArray<StructLine> StructLines { get; }
-    IList<HighlightArea> HighlightAreas { get; }
+    List<HighlightArea> HighlightAreas { get; }
     CompactBspTree BspTree { get; }
     IRandom Random { get; }
     // Used for randomization that should not affect demos
