8411519b02336dc561b8b460b248655182d2d27c
Nick <stlaurent.nicholas@gmail.com>
2025-10-14 06:14:33 -0400

    fix warnings

--- Core/Render/OpenGL/Renderers/Legacy/World/StaticWallClipAlphaShader.cs
+++ Core/Render/OpenGL/Renderers/Legacy/World/StaticWallClipAlphaShader.cs
@@ -1,6 +1,6 @@
 ﻿namespace Helion.Render.OpenGL.Renderers.Legacy.World;
 
-internal class StaticWallClipAlphaShader : StaticShader
+internal sealed class StaticWallClipAlphaShader : StaticShader
 {
     public StaticWallClipAlphaShader() : base("WallClipAlpha")
     {
