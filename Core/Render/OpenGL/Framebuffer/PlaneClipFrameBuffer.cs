7bef6a7600c4581cedd3efa721afee7fe0dbf257
Nick <42744211+nstlaurent@users.noreply.github.com>
2025-10-20 19:20:44 -0400

    correctly initialize depth values so alpha walls aren't discarded when sampling unwritten pixels (#1353)

--- Core/Render/OpenGL/Framebuffer/PlaneClipFrameBuffer.cs
+++ Core/Render/OpenGL/Framebuffer/PlaneClipFrameBuffer.cs
@@ -108,7 +108,7 @@ public class PlaneClipFrameBuffer : IDisposable
         if (m_ownsDepthTexture)
             GL.Clear(ClearBufferMask.DepthBufferBit);
 
-        var clear = stackalloc float[4] { -255, -255, -255, -255 };
+        var clear = stackalloc float[4] { -255, 1e30f, -255, 1e30f };
         GL.ClearBuffer(ClearBuffer.Color, m_clearBufferIndex, clear);
 
         if (m_ownsDepthTexture)
