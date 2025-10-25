1188e7e453f80dbe0cdf42c9c97fc32917f558d6
Nick <stlaurent.nicholas@gmail.com>
2025-10-25 05:41:32 -0400

    fix amd shader compilation

--- Core/Render/OpenGL/Renderers/Legacy/World/Shader/VertexFunction.cs
+++ Core/Render/OpenGL/Renderers/Legacy/World/Shader/VertexFunction.cs
@@ -23,8 +23,8 @@ public static class VertexFunction
                 vec2 uvGap = vec2(VertexGapX / texSize.x, VertexGapY / texSize.y);
 
                 // Currently don't clamp when uv coordinates are flipped (eg topLeft.u > bottomRight.u). This happens in UDMF when scale x/y is negative.
-                uvClampMinFrag = mix(uvClampMinFrag, uvFrag + uvGap, topLeft == 1 && uvFlags == 0);
-                uvClampMaxFrag = mix(uvClampMaxFrag, uvFrag - uvGap, topLeft == 0 && uvFlags == 0);
+                uvClampMinFrag = mix(uvClampMinFrag, uvFrag + uvGap, float(topLeft == 1 && uvFlags == 0));
+                uvClampMaxFrag = mix(uvClampMaxFrag, uvFrag - uvGap, float(topLeft == 0 && uvFlags == 0));
             }
     ";
 
