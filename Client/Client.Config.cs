a1255671ee0c0442428ffd3e549696ad77678007
Nick <stlaurent.nicholas@gmail.com>
2025-10-31 16:07:18 -0400

    improve hud autoscale calculation

--- Client/Client.Config.cs
+++ Client/Client.Config.cs
@@ -42,7 +42,7 @@ public partial class Client
         if (!m_config.Hud.AutoScale)
             return;
 
-        int ratio = Math.Clamp((int)Math.Ceiling(m_window.ClientSize.Y / 799.0), 1, 10);
+        int ratio = Math.Clamp((int)Math.Ceiling(m_window.ClientSize.Y / 360.0), 1, 10);
         m_config.Hud.Scale.Set(ratio);
     }
 
