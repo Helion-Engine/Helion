ac01124bbfd9371760fe66ddcd8f3f1f4f2f285e
Nick <stlaurent.nicholas@gmail.com>
2025-11-13 19:58:47 -0500

    remove handle alloc and reliance on data version and use fixed pointers intead

--- Core/Render/OpenGL/Buffer/ArrayBufferObject.cs
+++ Core/Render/OpenGL/Buffer/ArrayBufferObject.cs
@@ -8,8 +8,14 @@ public abstract class ArrayBufferObject<T> : BufferObject<T> where T : struct
     protected override BufferTarget Target => BufferTarget.ArrayBuffer;
     protected abstract BufferUsageHint Hint { get; }
 
-    protected ArrayBufferObject(string objectLabel, int capacity = DefaultCapacity) : base(objectLabel, capacity)
+    private IntPtr m_ptr;
+
+    protected unsafe ArrayBufferObject(string objectLabel, int capacity = DefaultCapacity) : base(objectLabel, capacity)
     {
+        fixed (T* ptr = &Data.Data[0])
+        {
+            m_ptr = (IntPtr)ptr;
+        }
     }
 
     protected override void PerformUpload()
@@ -22,20 +28,24 @@ public abstract class ArrayBufferObject<T> : BufferObject<T> where T : struct
         GL.BufferData(Target, BytesPerElement * Data.Capacity, Data.Data, Hint);
     }
 
-    protected override void BufferSubData(int index, int length)
+    protected unsafe override void BufferSubData(int index, int length)
     {
-        // If the underlying array was resized then the new array needs to be uploaded
-        // This should be handled with BufferObject.UploadIfNeeded
-        if (m_dataVersion != Data.Version)
+        fixed (T* buffer = &Data.Data[0])
         {
-            Uploaded = false;
-            return;
+            var ptr = (IntPtr)buffer;
+            // If the underlying array was resized then the new array needs to be uploaded
+            // This should be handled with BufferObject.UploadIfNeeded
+            if (ptr != m_ptr)
+            {
+                m_ptr = ptr;
+                Uploaded = false;
+                return;
+            }
+
+            IntPtr offset = new(BytesPerElement * index);
+            int size = BytesPerElement * length;
+
+            GL.BufferSubData(Target, offset, size, ptr + (BytesPerElement * index));
         }
-
-        IntPtr offset = new(BytesPerElement * index);
-        int size = BytesPerElement * length;
-        IntPtr ptr = GetVboArray() + (BytesPerElement * index);
-
-        GL.BufferSubData(Target, offset, size, ptr);
     }
 }
