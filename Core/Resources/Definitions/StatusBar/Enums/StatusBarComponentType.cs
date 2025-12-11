894231299351d379ef6ab3d31d446b4658576636
Florina D. I. Mushin <heylasfas@gmail.com>
2025-12-11 11:55:54 -0700

    Implement SBARDEF up to v1.1.0 (#1425)

---
+++ Core/Resources/Definitions/StatusBar/Enums/StatusBarComponentType.cs
@@ -0,0 +1,17 @@
+namespace Helion.Resources.Definitions.StatusBar.Enums;
+
+public enum StatusBarComponentType
+{
+    Unknown = 0,
+    StatTotals,
+    Time,
+    Coordinates,
+    Speedometer,
+    LevelTitle,
+    FpsCounter,
+    Message,
+    AnnounceLevelTitle,
+    RenderStats,
+    CommandHistory,
+    Chat
+}
