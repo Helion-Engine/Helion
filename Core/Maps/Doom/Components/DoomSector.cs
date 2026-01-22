e0a03386e372abd980059896e915831253780ac3
Nick <42744211+nstlaurent@users.noreply.github.com>
2026-01-22 15:43:01 -0500

    fix boom generalized sector damage not working (#1476)

--- Core/Maps/Doom/Components/DoomSector.cs
+++ Core/Maps/Doom/Components/DoomSector.cs
@@ -13,7 +13,6 @@ public class DoomSector : ISector
     public short LightLevel { get; }
     public ushort Tag { get; }
     public readonly int SectorType;
-    public SectorDamageSpecial? SectorDamageSpecial { get; set; }
 
     internal DoomSector(int id, short floorZ, short ceilingZ, string floorTexture, string ceilingTexture,
         short lightLevel, int sectorType, ushort tag)
