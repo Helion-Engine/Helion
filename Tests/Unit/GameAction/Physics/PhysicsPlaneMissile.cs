dbad783340890b938b15d4f25535df7f78e1c68c
Nick <stlaurent.nicholas@gmail.com>
2026-02-15 14:04:53 -0500

    fix cases of missiles not being destroyed when clipped with planes

--- Tests/Unit/GameAction/Physics/PhysicsPlaneMissile.cs
+++ Tests/Unit/GameAction/Physics/PhysicsPlaneMissile.cs
@@ -4,8 +4,8 @@ using Helion.World.Entities.Players;
 using Helion.World.Geometry.Sectors;
 using Helion.World.Impl.SinglePlayer;
 using Helion.World.Physics;
-using System;
-using System.Reflection;
+using Helion.World.Special.Specials;
+using System.Linq;
 using Xunit;
 
 namespace Helion.Tests.Unit.GameAction;
@@ -20,7 +20,6 @@ public class PhysicsPlaneMissile
     {
         World = WorldAllocator.LoadMap("Resources/physicsplanemissile.zip", "physicsplanemissile.WAD", "MAP01", GetType().Name, (world) => { }, IWadType.Doom2, cacheWorld: false);
     }
-
 
     [Fact(DisplayName = "NoBlockmap Missile on moving floor doesn't explode or move")]
     public void NoBlockmapMissile()
@@ -54,8 +53,8 @@ public class PhysicsPlaneMissile
         missile.Position.Z.Should().Be(sector.Floor.Z);
     }
 
-    [Fact(DisplayName = "Missile on moving floor doesn't explode and moves")]
-    public void FloorImpactsMissle()
+    [Fact(DisplayName = "Missile explodes when hitting moving floor without no blockmap")]
+    public void FloorImpactsMissile()
     {
         var missile = GameActions.CreateEntity(World, "*deh/entity151", (-288, 192, 32));
         missile.Flags.NoBlockmap().Should().BeFalse();
@@ -66,12 +65,67 @@ public class PhysicsPlaneMissile
         missile.Flags.Missile().Should().BeFalse();
     }
 
+    [Fact(DisplayName = "Missile explodes when hitting moving floor with no blockmap and xy velocity (entity hits floor)")]
+    public void FloorImpactsNoBlockMapMissileXYByEntity()
+    {
+        var missile = GameActions.CreateEntity(World, "Rocket", (-288, 192, 16));
+        missile.Velocity = new(1, 0, 0);
+        missile.Flags.NoBlockmap().Should().BeTrue();
+        missile.Flags.Missile().Should().BeTrue();
+        var sector = MoveUp();
+        sector.Floor.Z.Should().Be(48);
+        missile.IsDisposed.Should().BeTrue();
+        missile.Flags.Missile().Should().BeFalse();
+    }
+
+
+    [Fact(DisplayName = "Missile explodes when hitting moving floor with no blockmap and xy velocity (floor hits entity)")]
+    public void FloorImpactsNoBlockMapMissileXYByFloor()
+    {
+        var missile = GameActions.CreateEntity(World, "Rocket", (-288, 192, 16));
+        missile.Velocity = new(1, 0, 0);
+        missile.Flags.NoBlockmap().Should().BeTrue();
+        missile.Flags.Missile().Should().BeTrue();
+        var sector = MoveUpFast();
+        GameActions.TickWorld(World, 35);
+        sector.Floor.Z.Should().Be(48);
+        missile.IsDisposed.Should().BeTrue();
+        missile.Flags.Missile().Should().BeFalse();
+    }
+
+    [Fact(DisplayName = "Missile explodes when hitting moving floor without no blockmap and z velocity (floor hits entity)")]
+    public void FloorImpactsNoBlockMapMissileZByFloor()
+    {
+        var missile = GameActions.CreateEntity(World, "*deh/entity151", (-288, 192, 32));
+        missile.Velocity = new(0, 0, -0.01);
+        missile.Flags.NoBlockmap().Should().BeFalse();
+        missile.Flags.Missile().Should().BeTrue();
+        // This makes the floor movement hit the entity instead of the entity movement hitting the floor
+        var sector = MoveUpFast();
+        GameActions.TickWorld(World, 35);
+        sector.Floor.Z.Should().Be(48);
+        missile.IsDisposed.Should().BeTrue();
+        missile.Flags.Missile().Should().BeFalse();
+    }
+
     [Fact(DisplayName = "Crusher doesn't affect missile that isn't moving")]
-    public void CrusherMissile()
+    public void CrusherMissileNoEffect()
     {
         var missile = GameActions.GetEntity(World, 1);
         var sector = Crush();
         missile.Flags.Missile().Should().BeTrue();
+        sector.Floor.Z.Should().Be(16);
+        sector.Ceiling.Z.Should().Be(24);
+    }
+
+    [Fact(DisplayName = "Crusher explodes missiles")]
+    public void CrusherMissile()
+    {
+        var missile = GameActions.GetEntity(World, 1);
+        missile.Velocity.X = 1;
+        missile.Flags.Missile().Should().BeTrue();
+        var sector = Crush();
+        missile.Flags.Missile().Should().BeFalse();
         sector.Floor.Z.Should().Be(16);
         sector.Ceiling.Z.Should().Be(24);
     }
@@ -88,6 +142,16 @@ public class PhysicsPlaneMissile
     {
         var sector = GameActions.GetSectorByTag(World, 1);
         GameActions.ActivateLine(World, Player, 15, ActivationContext.UseLine).Should().BeTrue();
+        GameActions.RunSectorPlaneSpecial(World, sector);
+        return sector;
+    }
+
+    private Sector MoveUpFast()
+    {
+        var sector = GameActions.GetSectorByTag(World, 1);
+        GameActions.ActivateLine(World, Player, 15, ActivationContext.UseLine).Should().BeTrue();
+        var moveSpecial = (SectorMoveSpecial)World.SpecialManager.GetSpecials().First(x => x is SectorMoveSpecial);
+        moveSpecial.MoveSpeed = 255;
         GameActions.RunSectorPlaneSpecial(World, sector);
         return sector;
     }
