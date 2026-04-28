bd30310d6b775f8bf86309e0c6c2b101c78fb7ad
Nick <stlaurent.nicholas@gmail.com>
2026-04-28 13:24:47 -0400

    correct sizing for buffers and add assert checks for overflows
    add command to dump buffer data to text files

---
+++ Client/Client.Commands.Developer.cs
@@ -0,0 +1,13 @@
+﻿using Helion.Util.Consoles;
+using Helion.Util.Consoles.Commands;
+
+namespace Helion.Client;
+
+public partial class Client
+{
+    [ConsoleCommand("dumpbuffers", "Writes OpenGL TBO buffer data")]
+    private void DumpBuffers(ConsoleCommandEventArgs args)
+    {
+        m_window.Renderer.DumpBuffers();
+    }
+}
