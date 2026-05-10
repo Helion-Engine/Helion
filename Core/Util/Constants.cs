056258d5ed0a8dedf7c01a63da7db07360d389de
Nick <42744211+nstlaurent@users.noreply.github.com>
2026-05-10 08:03:24 -0400

    support both fixed index and user configured extra light when negative for palette rendering (#1611)

--- Core/Util/Constants.cs
+++ Core/Util/Constants.cs
@@ -336,19 +336,8 @@ public static class Constants
         public const int MiddleWallOffset = 1;
         public const int LowerWallOffset = 2;
         public const int ColorMapCount = 32;
-    }
-
-    public static class ColormapBuffer
-    {
-        public const int DarkIndex = 0;
-        public const int FullBrightIndex = 1;
-        public const int ColorMapStartIndex = 2;
-        public const int BufferSize = 3;
-        public const int SectorIndexStart = ColorMapStartIndex + ColorMapCount;
-        public const int FloorOffset = 0;
-        public const int CeilingOffset = 1;
-        public const int WallOffset = 2;
-        public const int ColorMapCount = 32;
+        public const int LightLevels = 48;
+        public const int LightLevelsIndexMax = LightLevels - 1;
     }
 
     public static class ColorMapIndex
