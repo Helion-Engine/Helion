89418a95eedfdf9ea2cb004e222997513c31538e
Nick <stlaurent.nicholas@gmail.com>
2026-05-22 14:30:30 -0400

    fix setting alternate dictionary in second constructor

--- Core/World/Entities/Inventories/Inventory.cs
+++ Core/World/Entities/Inventories/Inventory.cs
@@ -81,6 +81,7 @@ public sealed class Inventory
         Owner = owner;
         EntityDefinitionComposer = composer;
         Weapons = new Weapons(this, WorldStatic.World.GameInfo.WeaponSlots, composer);
+        ItemsBySpan = Items.GetAlternateLookup<ReadOnlySpan<char>>();
 
         for (int i = 0; i < playerModel.Inventory.Items.Count; i++)
         {
