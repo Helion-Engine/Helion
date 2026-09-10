50951da8eabfb2b5e63b9a8e33d1eb6774276fe2
Nick <stlaurent.nicholas@gmail.com>
2026-09-10 08:16:28 -0400

    remove test code

--- Core/Render/OpenGL/Renderers/Legacy/World/Geometry/Static/RenderGeometry.cs
+++ Core/Render/OpenGL/Renderers/Legacy/World/Geometry/Static/RenderGeometry.cs
@@ -1,5 +1,4 @@
 ﻿using Helion.Util.Container;
-using System;
 using System.Collections.Generic;
 
 namespace Helion.Render.OpenGL.Renderers.Legacy.World.Geometry.Static;
@@ -7,28 +6,10 @@ namespace Helion.Render.OpenGL.Renderers.Legacy.World.Geometry.Static;
 public class RenderGeometry
 {
     private readonly GeometryTypeLookup<List<GeometryData>> m_lookup = new(() => new List<GeometryData>());
-    private readonly Comparison<GeometryData> m_sortByTextureArray;
-
-    public RenderGeometry()
-    {
-        m_sortByTextureArray = SortByTextureArray;
-    }
 
     public void AddGeometry(GeometryType type, GeometryData data)
     {
-        var geometry = m_lookup.Get(type);
-        geometry.Add(data);
-        geometry.Sort(m_sortByTextureArray);
-    }
-
-    private static int SortByTextureArray(GeometryData x, GeometryData y)
-    {
-        if (x.Texture.IsArray && !y.Texture.IsArray)
-            return 0;
-        if (!x.Texture.IsArray && y.Texture.IsArray)
-            return 1;
-
-        return x.Texture.Dimension.Height.CompareTo(y.Texture.Dimension.Height);
+        m_lookup.Get(type).Add(data);
     }
 
     public List<GeometryData> GetGeometry(GeometryType type)
